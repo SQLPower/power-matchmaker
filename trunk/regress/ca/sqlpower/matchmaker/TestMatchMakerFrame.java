29c8d8e32f6ef00474f7860f8b8f44ca67cc862b
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-09-15 21:50:08 +0000

    added unit test for matchmakerframe

---
+++ trunk/regress/ca/sqlpower/matchmaker/TestMatchMakerFrame.java
@@ -0,0 +1,21 @@
+package ca.sqlpower.matchmaker;
+
+import junit.framework.TestCase;
+import ca.sqlpower.matchmaker.swingui.MatchMakerFrame;
+
+public class TestMatchMakerFrame extends TestCase {
+
+	private MatchMakerFrame mf;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mf = MatchMakerFrame.getMainInstance();
+	}
+
+	public void testMatchMakerFrame() {
+		assertNotNull(mf.getArchitectSession());
+		assertNotNull(mf.getArchitectSession().getUserSettings());
+	}
+
+}
