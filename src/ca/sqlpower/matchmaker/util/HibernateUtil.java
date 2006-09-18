a52de790934986b669b340c54ca410a9b18bc4b3
ian <ian@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-09-18 21:09:13 +0000

    Some (initially only one) Hibernate utilities.

---
+++ src/ca/sqlpower/matchmaker/util/HibernateUtil.java
@@ -0,0 +1,38 @@
+package ca.sqlpower.matchmaker.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ca.sqlpower.matchmaker.DefParamHome;
+
+/**
+ * Some Utils to encapsulate common Hibernate operations.
+ */
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory;
+
+	private static final Log log = LogFactory.getLog(DefParamHome.class);
+
+	static {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			sessionFactory = new Configuration().configure()
+					.buildSessionFactory();
+		} catch (Throwable ex) {
+			log.error("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	/**
+	 * Get a Hibernate SessionFactory object, without reference to
+	 * whether it is created, looked up, etc.
+	 * @return
+	 */
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
