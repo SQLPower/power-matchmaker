319c60fff9f00e9b8d1ac46a3515075c9fc6c687
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-09-18 20:14:14 +0000

    added login dialog for the app

--- src/ca/sqlpower/matchmaker/swingui/LoginFrame.java
+++ src/ca/sqlpower/matchmaker/swingui/LoginFrame.java
@@ -1,6 +1,13 @@
 package ca.sqlpower.matchmaker.swingui;
 
+
+
+
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
@@ -9,10 +16,11 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.event.ListDataEvent;
@@ -20,9 +28,18 @@ import javax.swing.event.ListDataListener;
 
 import org.apache.log4j.Logger;
 
+import ca.sqlpower.architect.ArchitectDataSource;
 import ca.sqlpower.architect.ArchitectException;
-import ca.sqlpower.architect.swingui.CommonCloseAction;
+import ca.sqlpower.architect.SQLColumn;
+import ca.sqlpower.architect.SQLDatabase;
+import ca.sqlpower.architect.SQLTable;
+import ca.sqlpower.architect.ddl.DDLUtils;
+import ca.sqlpower.architect.swingui.ASUtils;
 import ca.sqlpower.architect.swingui.ConnectionComboBoxModel;
+import ca.sqlpower.architect.swingui.ListerProgressBarUpdater;
+import ca.sqlpower.architect.swingui.Populator;
+import ca.sqlpower.architect.swingui.ASUtils.LabelValueBean;
+import ca.sqlpower.sql.SQL;
 
 import com.jgoodies.forms.builder.ButtonBarBuilder;
 import com.jgoodies.forms.builder.PanelBuilder;
@@ -39,6 +56,9 @@ public class LoginFrame extends JDialog {
 	private JCheckBox refreshDB;
 	private JCheckBox refreshPL;
 	private JLabel dbSourceName;
+	protected JButton loginButton = new JButton();
+	protected JProgressBar progressBar = new JProgressBar();
+	protected ArchitectDataSource dbSource;
 
 	private JComponent panel;
 	private Action helpLoginAction = new AbstractAction(){
@@ -48,11 +68,115 @@ public class LoginFrame extends JDialog {
 
 		}};
 
-	private Action loginAction = new AbstractAction(){
+	private ActionListener loginAction = new LoginAction();
+
+	private class LoginAction extends Populator implements ActionListener {
+
+		SQLDatabase db = null;
+		boolean login = false;
+
+		@Override
+		public void cleanup() {
+			progressBar.setVisible(false);
+			if ( db != null && db.isPopulated() && login ) {
+				SQLTable defParam = null;
+				SQLColumn schemaVersionCol = null;
+				try {
+
+					defParam = db.getTableByName("DEF_PARAM");
+					schemaVersionCol = defParam.getColumnByName("SCHEMA_VERSION");
+
+					String ver = null;
+			        Statement stmt = null;
+			        ResultSet rs = null;
+			        try {
+			            stmt = db.getConnection().createStatement();
+			            String defParamTableName = DDLUtils.toQualifiedName(
+			            		defParam.getCatalogName(),
+			            		defParam.getSchemaName(),
+			            		defParam.getName());
+			            StringBuffer sql = new StringBuffer();
+			            sql.append("SELECT ");
+			            sql.append(schemaVersionCol.getName());
+			            sql.append(" FROM ");
+			            sql.append(defParamTableName);
+			            rs = stmt.executeQuery(sql.toString());
+
+			            if (rs.next()) {
+			            	 ver = rs.getString(1);
+			            }
+
+			            logger.debug("connected to "+db.getName()+
+			            		"  schema version: " + ver );
+			            		LoginFrame.this.setVisible(false);
+			        } catch (SQLException e) {
+			            throw new ArchitectException("could not read def_param",e);
+			        } finally {
+			        	try {
+			        		if (rs != null)
+			        			rs.close();
+			        		if (stmt != null)
+			        			stmt.close();
+			        	} catch (SQLException e) {
+			        		e.printStackTrace();
+			        	}
+
+			        }
+
+
+				} catch (ArchitectException e) {
+					ASUtils.showExceptionDialogNoReport(LoginFrame.this,
+							"Pl Schema Access Error", e );
+				} finally {
+					loginButton.setEnabled(true);
+				}
+			}
+			loginButton.setEnabled(true);
+		}
+
+		@Override
+		public void doStuff() throws Exception {
+			loginButton.setEnabled(false);
+			login = false;
+			try {
+				ListerProgressBarUpdater progressBarUpdater =
+					new ListerProgressBarUpdater(progressBar, this);
+				new javax.swing.Timer(100, progressBarUpdater).start();
+				db.populate();
+				login = true;
+			} catch (ArchitectException e) {
+				logger.debug(
+					"Unexpected exception in ConnectionListener",	e);
+				ASUtils.showExceptionDialogNoReport(LoginFrame.this,
+						"Unexpected exception in ConnectionListener",
+						e );
+                login = false;
+			}
+		}
 
 		public void actionPerformed(ActionEvent e) {
+			loginButton.setEnabled(false);
+			if ( LoginFrame.this.dbSource == null ) {
+				JOptionPane.showMessageDialog(LoginFrame.this,
+						"Please select a database connection first!",
+						"Unknown database connection",
+						JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			db = new SQLDatabase(dbSource);
+			try {
+				progressMonitor = db.getProgressMonitor();
+				new Thread(this).start();
+			} catch (ArchitectException e1) {
+				ASUtils.showExceptionDialogNoReport(LoginFrame.this,
+						"Connection Error", e1 );
+			} finally {
+			}
 
-		}};
+		}
+
+	}
+
 
 
 	public LoginFrame()
@@ -84,11 +208,11 @@ public class LoginFrame extends JDialog {
 			}
 
 			public void contentsChanged(ListDataEvent e) {
-				if ( dbSourceName != null ) {
-					dbSourceName.setText(((ConnectionComboBoxModel) (e.getSource())).getSelectedItem().toString());
-					System.out.println("["+((ConnectionComboBoxModel) (e.getSource())).getSelectedItem().toString()+"]");
-				}
-
+				ArchitectDataSource dbSource = (ArchitectDataSource) ((ConnectionComboBoxModel) (e.getSource())).getSelectedItem();
+				dbSourceName.setText(dbSource.getName());
+				userID.setText(dbSource.getUser());
+				password.setText(dbSource.getPass());
+				LoginFrame.this.dbSource = dbSource;
 			}});
 		dbList = new JComboBox(connectionModel);
 		JLabel dbSourceName1 = new JLabel("Database source name:");
@@ -111,9 +235,13 @@ public class LoginFrame extends JDialog {
 		bbBuilder.addRelatedGap();
 		bbBuilder.addGlue();
 
-		JButton loginButton = new JButton(loginAction);
+
+		loginButton.addActionListener(loginAction);
 		loginButton.setText("Login");
-		JButton cancelButton = new JButton(new CommonCloseAction(LoginFrame.this));
+		JButton cancelButton = new JButton(new AbstractAction(){
+			public void actionPerformed(ActionEvent e) {
+				LoginFrame.this.setVisible(false);
+			}});
 		cancelButton.setText("Cancel");
 		bbBuilder.addGridded(loginButton);
 		bbBuilder.addUnrelatedGap();
@@ -135,6 +263,8 @@ public class LoginFrame extends JDialog {
 		pb.add(passwordLabel,cc.xy(2,10,"r,c"));
 		pb.add(password,cc.xyw(4,10,2,"l,c"));
 
+		progressBar.setVisible(false);
+		pb.add(progressBar,cc.xyw(2,12,6));
 		pb.add(new JLabel("Refresh DB Schema"), cc.xy(2, 14,"r,c"));
 		pb.add(refreshDB,cc.xy(4,14));
 		pb.add(new JLabel("Refresh PL Schema"), cc.xy(5, 14, "r,c"));
@@ -163,6 +293,10 @@ public class LoginFrame extends JDialog {
 		    	d.setVisible(true);
 		    }
 		});
+	}
+
+	public ArchitectDataSource getDbSource() {
+		return dbSource;
 	}
 
 }
