afb0be5bd9258d872c88e5bf27022599fe3f236f
ian <ian@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-09-25 16:57:53 +0000

    Organize imports

--- src/ca/sqlpower/matchmaker/swingui/MatchMakerTreeMouseListener.java
+++ src/ca/sqlpower/matchmaker/swingui/MatchMakerTreeMouseListener.java
@@ -1,15 +1,12 @@
 package ca.sqlpower.matchmaker.swingui;
 
 import java.awt.Component;
-import java.awt.Menu;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JTree;
@@ -18,8 +15,8 @@ import javax.swing.tree.TreePath;
 import ca.sqlpower.matchmaker.hibernate.PlFolder;
 import ca.sqlpower.matchmaker.hibernate.PlMatch;
 import ca.sqlpower.matchmaker.hibernate.PlMatchGroup;
-import ca.sqlpower.matchmaker.swingui.action.EditMatchGroupAction;
 import ca.sqlpower.matchmaker.swingui.action.DeleteMatchGroupAction;
+import ca.sqlpower.matchmaker.swingui.action.EditMatchGroupAction;
 import ca.sqlpower.matchmaker.swingui.action.NewMatchGroupAction;
 import ca.sqlpower.matchmaker.swingui.action.Refresh;
 
@@ -95,7 +92,7 @@ public class MatchMakerTreeMouseListener implements MouseListener {
                 f.pack();
                 f.setVisible(true);
             }}));
-		
+
 		m.add(new JMenuItem(new NewMatchGroupAction(match, getWindow())));
 	}
 
