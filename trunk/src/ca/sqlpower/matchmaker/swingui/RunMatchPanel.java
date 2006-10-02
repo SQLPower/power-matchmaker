ba30a7be3c8b9ebab1cd0d161c61a149f7a2aff8
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-10-02 21:00:21 +0000

    working on run match panel

--- trunk/src/ca/sqlpower/matchmaker/swingui/RunMatchPanel.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/RunMatchPanel.java
@@ -1,23 +1,46 @@
 package ca.sqlpower.matchmaker.swingui;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Transaction;
 
+import ca.sqlpower.architect.SQLDatabase;
+import ca.sqlpower.architect.swingui.ASUtils;
+import ca.sqlpower.architect.swingui.SaveDocument;
+import ca.sqlpower.architect.swingui.ASUtils.FileExtensionFilter;
 import ca.sqlpower.matchmaker.hibernate.PlMatch;
+import ca.sqlpower.matchmaker.util.HibernateUtil;
 
 import com.jgoodies.forms.builder.ButtonBarBuilder;
 import com.jgoodies.forms.builder.PanelBuilder;
@@ -30,13 +53,13 @@ public class RunMatchPanel extends JFrame{
     private static final Logger logger = Logger.getLogger(RunMatchPanel.class);
     private JTextField logFilePath;
     String lastAccessPath;
-    
+
     private JButton browse;
     private JCheckBox append;
     private JTextField recordsToProcess;
     private JTextField minWord;
     private JCheckBox debugMode;
-    private JCheckBox trancateCandDup;
+    private JCheckBox truncateCandDup;
     private JCheckBox sendEmail;
     private JButton viewLogFile;
     private JButton viewStats;
@@ -50,22 +73,21 @@ public class RunMatchPanel extends JFrame{
     private JButton runMatchEngine;
     private JButton exit;
     private JComboBox rollbackSegment;
-    
+
     private PlMatch plMatch;
-    
+
     public RunMatchPanel(PlMatch plMatch){
-        super();        
+        super();
         this.plMatch = plMatch;
         buildUI();
     }
-    
+
     private Action browseFileAction = new AbstractAction("...") {
 
         public void actionPerformed(ActionEvent e) {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setCurrentDirectory(new File(lastAccessPath));
-           //Change this to another filter
-           //fileChooser.addChoosableFileFilter(ASUtils.ARCHITECT_FILE_FILTER);
+            fileChooser.setSelectedFile(new File(lastAccessPath));
            int returnVal = fileChooser.showOpenDialog(RunMatchPanel.this);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     final File file = fileChooser.getSelectedFile();
@@ -74,41 +96,55 @@ public class RunMatchPanel extends JFrame{
                 }
 
         }};
-    
+
     public void buildUI(){
         FormLayout layout = new FormLayout(
                 "4dlu,fill:min(70dlu;default),4dlu,fill:200dlu:grow, 7dlu,4dlu,10dlu,45dlu,10dlu,10dlu,10dlu",
-                "10dlu,12dlu,10dlu,10dlu,3dlu,10dlu,3dlu,10dlu,30dlu,40dlu,10dlu,10dlu"); 
-
+                "10dlu,12dlu,10dlu,10dlu,3dlu,10dlu,3dlu,10dlu,30dlu,16dlu,4dlu,16dlu,4dlu");
+        //		 1     2     3     4     5    6     7    8     9     10    11    12   13
         PanelBuilder pb;
         JPanel p = logger.isDebugEnabled() ? new FormDebugPanel(layout) : new JPanel(layout);
         pb = new PanelBuilder(layout, p);
 
         CellConstraints cc = new CellConstraints();
-        
+
         logFilePath = new JTextField();
         browse = new JButton(browseFileAction);
         append = new JCheckBox();
-        recordsToProcess = new JTextField(5);        
+        recordsToProcess = new JTextField(5);
         minWord = new JTextField(5);
         minWord.setText("0");
         debugMode = new JCheckBox();
-        trancateCandDup = new JCheckBox();
+        truncateCandDup = new JCheckBox();
         sendEmail = new JCheckBox();
-        viewLogFile = new JButton();
-        viewStats = new JButton();
-        showCommand = new JButton();
+        viewLogFile = new JButton(new ShowLogFileAction());
+        viewStats = new JButton(new ViewStats(plMatch));
+        showCommand = new JButton(new ShowCommandAction(plMatch,RunMatchPanel.this));
         viewMatchResults = new JButton();
         startWordCount = new JButton();
         wordCountResults = new JButton();
         maintainTranslateWords = new JButton();
         validateMatches = new JButton();
-        save = new JButton();
-        runMatchEngine = new JButton();
-        exit = new JButton();
+        save = new JButton(new AbstractAction(){
+			public void actionPerformed(ActionEvent e) {
+				applyChange();
+				Transaction tx = HibernateUtil.primarySession().beginTransaction();
+				HibernateUtil.primarySession().flush();
+				tx.commit();
+			}});
+        save.setText("Save");
+
+        runMatchEngine = new JButton(new RunEngineAction(plMatch,RunMatchPanel.this));
+        exit = new JButton(new AbstractAction(){
+			public void actionPerformed(ActionEvent e) {
+				RunMatchPanel.this.setVisible(false);
+				RunMatchPanel.this.dispose();
+			}});
+        exit.setText("Exit");
+
         rollbackSegment = new JComboBox();
-        
-        
+
+
         if ( plMatch != null ) {
             String logFileName = plMatch.getMatchLogFileName();
             File file;
@@ -120,16 +156,21 @@ public class RunMatchPanel extends JFrame{
             }
             lastAccessPath = file.getAbsolutePath();
             logFilePath.setText(logFileName);
-            
-            recordsToProcess.setText(String.valueOf(plMatch.getMatchProcessCnt()));
-            
-            // fixme
-            debugMode.setSelected(Boolean.valueOf(plMatch.getMatchRunStatus()));
+            append.setSelected(plMatch.isMatchAppendToLogInd());
+            if ( plMatch.getMatchProcessCnt() != null ) {
+            	recordsToProcess.setText(String.valueOf(plMatch.getMatchProcessCnt()));
+            } else {
+            	recordsToProcess.setText("0");
+            }
+            debugMode.setSelected(plMatch.isMatchDebugModeInd());
+            truncateCandDup.setSelected(plMatch.isTruncateCandDupInd());
+            sendEmail.setSelected(plMatch.isMatchSendEmailInd());
+            rollbackSegment.setSelectedItem(plMatch.getMatchRollbackSegmentName());
         } else {
-            recordsToProcess.setText("0");        
+            recordsToProcess.setText("0");
         }
-        
-        
+
+
         pb.add(new JLabel("Log File:"), cc.xy(2,2,"r,f"));
         pb.add(logFilePath, cc.xy(4,2,"f,f"));
         pb.add(browse, cc.xy(5,2,"r,f"));
@@ -138,17 +179,17 @@ public class RunMatchPanel extends JFrame{
         pb.add(new JLabel("Debug Mode?"), cc.xyw(6,4,3,"l,c"));
         pb.add(debugMode, cc.xy(10,4,"r,c"));
         pb.add(new JLabel("Truncate Cand Dup?"), cc.xyw(6,6,3,"l,c"));
-        pb.add(trancateCandDup, cc.xy(10,6,"r,c"));
+        pb.add(truncateCandDup, cc.xy(10,6,"r,c"));
         pb.add(new JLabel("Send E-mails"), cc.xyw(6,8,3,"l,c"));
         pb.add(sendEmail, cc.xy(10,8,"r,c"));
-        
+
         pb.add(new JLabel("Rollback Segment:"), cc.xy(2,4,"r,c"));
         pb.add(rollbackSegment, cc.xy(4,4));
         pb.add(new JLabel("Records to Process:"), cc.xy(2,6,"r,c"));
         pb.add(recordsToProcess, cc.xy(4,6,"l,c"));
         pb.add(new JLabel("Min Word Count Freq:"), cc.xy(2,8,"r,c"));
         pb.add(minWord, cc.xy(4,8,"l,c"));
-        
+
         ButtonBarBuilder bb1 = new ButtonBarBuilder();
         bb1.addGridded(viewLogFile);
         bb1.addRelatedGap();
@@ -163,15 +204,369 @@ public class RunMatchPanel extends JFrame{
         bb1.addRelatedGap();
         bb1.addGlue();
         pb.add(bb1.getPanel(), cc.xyw(2,10,10));
-        
-        
+
+
+        ButtonBarBuilder bb2 = new ButtonBarBuilder();
+        bb2.addGridded(save);
+        bb2.addRelatedGap();
+        bb2.addGlue();
+        bb2.addGridded(runMatchEngine);
+        bb2.addRelatedGap();
+        bb2.addGlue();
+        bb2.addGridded(exit);
+        bb2.addRelatedGap();
+        bb2.addGlue();
+        pb.add(bb2.getPanel(), cc.xyw(2,12,10));
+
         getContentPane().add(pb.getPanel());
     }
-    
-    private void setupUI() {
-        
+
+    private void applyChange() {
+    	plMatch.setMatchDebugModeInd(debugMode.isSelected());
+    	plMatch.setTruncateCandDupInd(truncateCandDup.isSelected());
+    	plMatch.setMatchSendEmailInd(sendEmail.isSelected());
+    	plMatch.setMatchLogFileName(logFilePath.getText());
+    	plMatch.setMatchAppendToLogInd(append.isSelected());
+    	plMatch.setMatchProcessCnt(Long.valueOf(recordsToProcess.getText()));
     }
-    
+
+    public class EngineRunner implements Runnable {
+
+		public void run() {
+		}
+
+    }
+
+    public class StreamGobbler extends Thread
+    {
+    	InputStream is;
+    	String type;
+    	AbstractDocument output;
+    	SimpleAttributeSet att;
+
+
+    	StreamGobbler(InputStream is, String type,
+    			AbstractDocument output,
+    			SimpleAttributeSet att ) {
+    		this.is = is;
+    		this.type = type;
+    		this.output = output;
+    		this.att = att;
+    	}
+
+    	public void run() {
+    		try {
+    			InputStreamReader isr = new InputStreamReader(is);
+    			BufferedReader br = new BufferedReader(isr);
+    			String line=null;
+    			while ( (line = br.readLine()) != null) {
+    				logger.debug(type + ">" + line);
+    				synchronized (output) {
+    					output.insertString(output.getLength(),line+"\n",att);
+					}
+    			}
+    		} catch (IOException ioe) {
+    			ioe.printStackTrace();
+    		} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+
+    private class ViewStats extends AbstractAction {
+    	private PlMatch match;
+
+		public ViewStats(PlMatch match) {
+    		super("View Status");
+    		this.match = match;
+    	}
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+    }
+
+    private class RunEngineAction extends AbstractAction {
+
+    	private PlMatch match;
+		private Component parent;
+		DefaultStyledDocument engineOutputDoc = new DefaultStyledDocument();
+		SimpleAttributeSet stdoutAtt = new SimpleAttributeSet();
+		SimpleAttributeSet stderrAtt = new SimpleAttributeSet();
+
+
+    	public RunEngineAction(PlMatch match, Component parent) {
+    		super("Run Match Engine");
+    		this.parent = parent;
+    		this.match = match;
+			StyleConstants.setForeground(stdoutAtt, Color.black);
+			StyleConstants.setFontFamily(stdoutAtt,"Courier New");
+			StyleConstants.setForeground(stderrAtt, Color.red);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			applyChange();
+			final String cmd = createCommand(match) + " USER_PROMPT=N";
+			final JDialog d = new JDialog();
+			d.setTitle("MatchMaker engine output:");
+
+			FormLayout layout = new FormLayout(
+					"4dlu,fill:400dlu:grow,4dlu", // columns
+					"4dlu,fill:400dlu:grow,4dlu,16dlu,4dlu"); // rows
+	    	//		 1    2                        3    4     5
+
+			PanelBuilder pb;
+			JPanel p = logger.isDebugEnabled() ?
+					new FormDebugPanel(layout) : new JPanel(layout);
+			pb = new PanelBuilder(layout, p);
+			CellConstraints cc = new CellConstraints();
+
+			JTextArea cmdText = new JTextArea(engineOutputDoc);
+			cmdText.setEditable(false);
+			cmdText.setWrapStyleWord(true);
+			cmdText.setLineWrap(true);
+			cmdText.setAutoscrolls(true);
+			pb.add(new JScrollPane(cmdText), cc.xy(2,2,"f,f"));
+
+			ButtonBarBuilder bbBuilder = new ButtonBarBuilder();
+
+			Action saveAsAction = new AbstractAction(){
+				public void actionPerformed(ActionEvent e) {
+					new SaveDocument(d,engineOutputDoc,
+							(FileExtensionFilter) ASUtils.TEXT_FILE_FILTER );
+				}
+			};
+			JButton saveAsButton = new JButton(saveAsAction );
+			saveAsButton.setText("Save As...");
+			bbBuilder.addGridded (saveAsButton);
+			bbBuilder.addRelatedGap();
+
+			JButton copyButton = new JButton(new AbstractAction(){
+				public void actionPerformed(ActionEvent e) {
+					StringSelection selection = null;
+					try {
+						selection = new StringSelection(
+								engineOutputDoc.getText(0,engineOutputDoc.getLength()));
+					} catch (BadLocationException e1) {
+						ASUtils.showExceptionDialog(d,
+								"Document Copy Error", e1 );
+					}
+					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+					clipboard.setContents( selection, selection );
+				}});
+			copyButton.setText("Copy to Clipboard");
+			bbBuilder.addGridded (copyButton);
+			bbBuilder.addRelatedGap();
+			bbBuilder.addGlue();
+
+			JButton cancelButton = new JButton(new AbstractAction(){
+				public void actionPerformed(ActionEvent e) {
+					d.setVisible(false);
+					d.dispose();
+				}});
+			cancelButton.setText("Close");
+			bbBuilder.addGridded(cancelButton);
+
+			pb.add(bbBuilder.getPanel(), cc.xy(2,4));
+			d.add(pb.getPanel());
+			d.pack();
+			d.setVisible(true);
+
+			try {
+				engineOutputDoc.remove(0,engineOutputDoc.getLength());
+			} catch (BadLocationException e1) {
+				ASUtils.showExceptionDialog(d,
+						"Document Display Error", e1 );
+			}
+
+			new Thread(new Runnable(){
+
+				public void run() {
+					Runtime rt = Runtime.getRuntime();
+					logger.debug("Execing " + cmd);
+					Process proc;
+					try {
+						proc = rt.exec(cmd);
+						StreamGobbler errorGobbler = new
+						StreamGobbler(proc.getErrorStream(),
+								"ERROR", engineOutputDoc, stderrAtt);
+
+						// any output?
+						StreamGobbler outputGobbler = new
+						StreamGobbler(proc.getInputStream(),
+								"OUTPUT", engineOutputDoc, stdoutAtt);
+
+						// kick them off
+						errorGobbler.start();
+						outputGobbler.start();
+
+						// any error message?
+						// any error???
+						int exitVal = proc.waitFor();
+						logger.debug("ExitValue: " + exitVal);
+
+						try {
+							engineOutputDoc.insertString(engineOutputDoc.getLength(),
+									"\nExecutable Return Code: " + exitVal+"\n",
+									stderrAtt);
+
+						} catch (BadLocationException e1) {
+							ASUtils.showExceptionDialog(d,
+									"Document Display Error", e1 );
+						}
+
+					} catch (Throwable  e1) {
+						ASUtils.showExceptionDialog(parent,
+								"Engine Run Time Error",e1);
+					}
+				}}).start();
+		}
+    }
+
+    private class ShowLogFileAction extends AbstractAction {
+
+    	public ShowLogFileAction() {
+    		super("Show Log File");
+    	}
+
+    	public void actionPerformed(ActionEvent e) {
+
+    		try {
+    			// FIXME: should be user configable!
+    			String defaultEditor = "Notepad";
+    			String[] cmd = new String[2];
+   				cmd[0] = defaultEditor;
+   				cmd[1] = logFilePath.getText();
+
+    			Runtime rt = Runtime.getRuntime();
+    			System.out.println("Execing " + cmd[0] + " " + cmd[1]);
+    			Process proc = rt.exec(cmd);
+    			int exitVal = proc.waitFor();
+    			System.out.println("ExitValue: " + exitVal);
+    		} catch (Throwable t) {
+    			t.printStackTrace();
+    		}
+
+		}
+    }
+
+
+
+    private class ShowCommandAction extends AbstractAction {
+
+    	private PlMatch match;
+		private Component parent;
+
+		public ShowCommandAction(PlMatch match, Component parent) {
+    		super("Show Command");
+    		this.match = match;
+    		this.parent = parent;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			applyChange();
+			final String cmd = createCommand(match);
+			final JDialog d = new JDialog();
+			d.setTitle("MatchMaker Command Line:");
+
+			final DefaultStyledDocument cmdDoc = new DefaultStyledDocument();
+			SimpleAttributeSet att = new SimpleAttributeSet();
+			StyleConstants.setForeground(att, Color.black);
+
+			try {
+				cmdDoc.insertString(0,createCommand(match),att );
+			} catch (BadLocationException e1) {
+				ASUtils.showExceptionDialog(d,"Unknown Document Error",e1);
+			}
+
+			FormLayout layout = new FormLayout(
+					"4dlu,fill:200dlu:grow,4dlu", // columns
+					"4dlu,fill:200dlu:grow,4dlu,16dlu,4dlu"); // rows
+	    	//		 1    2                        3    4     5
+
+			PanelBuilder pb;
+			JPanel p = logger.isDebugEnabled() ?
+					new FormDebugPanel(layout) : new JPanel(layout);
+			pb = new PanelBuilder(layout, p);
+			CellConstraints cc = new CellConstraints();
+
+			JTextArea cmdText = new JTextArea(cmdDoc);
+			cmdText.setEditable(false);
+			cmdText.setWrapStyleWord(true);
+			cmdText.setLineWrap(true);
+			pb.add(cmdText, cc.xy(2,2,"f,f"));
+
+			ButtonBarBuilder bbBuilder = new ButtonBarBuilder();
+
+			Action saveAsAction = new AbstractAction(){
+				public void actionPerformed(ActionEvent e) {
+					new SaveDocument(d,cmdDoc,
+							(FileExtensionFilter) ASUtils.BATCH_FILE_FILTER );
+				}
+			};
+			JButton saveAsButton = new JButton(saveAsAction );
+			saveAsButton.setText("Save As...");
+			bbBuilder.addGridded (saveAsButton);
+			bbBuilder.addRelatedGap();
+
+			JButton copyButton = new JButton(new AbstractAction(){
+				public void actionPerformed(ActionEvent e) {
+					StringSelection selection = new StringSelection(cmd);
+					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+					clipboard.setContents( selection, selection );
+				}});
+			copyButton.setText("Copy to Clipboard");
+			bbBuilder.addGridded (copyButton);
+			bbBuilder.addRelatedGap();
+			bbBuilder.addGlue();
+
+			JButton cancelButton = new JButton(new AbstractAction(){
+				public void actionPerformed(ActionEvent e) {
+					d.setVisible(false);
+				}});
+			cancelButton.setText("Cancel");
+			bbBuilder.addGridded(cancelButton);
+
+			pb.add(bbBuilder.getPanel(), cc.xy(2,4));
+			d.add(pb.getPanel());
+			d.pack();
+			d.setVisible(true);
+
+		}
+
+    }
+
+    protected String createCommand(PlMatch match) {
+    	/*
+		 * command sample:
+		 * "M:\Program Files\Power Loader Suite\Match_Oracle.exe"
+		 * MATCH="MATCH_CTV_ORGS" USER=PL/pl@arthur_test DEBUG=Y
+		 * TRUNCATE_CAND_DUP=N SEND_EMAIL=N APPEND_TO_LOG_IND=N
+		 * LOG_FILE="M:\Program Files\Power Loader Suite\Power Loader\script\MATCH_MATCH_CTV_ORGS.log"
+		 * SHOW_PROGRESS=10 PROCESS_CNT=1
+		 */
+		StringBuffer command = new StringBuffer();
+		SQLDatabase db = MatchMakerFrame.getMainInstance().getDatabase();
+		// FIXME: replace executable to user config
+		command.append("\"M:\\Program Files\\Power Loader Suite\\Match_ODBC.exe\"");
+		command.append(" MATCH=\"").append(match.getMatchId()).append("\"");
+		if ( db != null ) {
+			command.append(" USER=");
+			command.append(db.getDataSource().getUser());
+			command.append("/").append(db.getDataSource().getPass());
+			command.append("@").append(db.getDataSource().getName());
+		}
+		command.append(" DEBUG=").append(match.isMatchDebugModeInd()?"Y":"N");
+		command.append(" TRUNCATE_CAND_DUP=").append(match.isTruncateCandDupInd()?"Y":"N");
+		command.append(" SEND_EMAIL=").append(match.isMatchSendEmailInd()?"Y":"N");
+		command.append(" APPEND_TO_LOG_IND=").append(match.isMatchAppendToLogInd()?"Y":"N");
+		command.append(" LOG_FILE=\"").append(match.getMatchLogFileName()).append("\"");
+		command.append(" SHOW_PROGRESS=").append(match.getMatchShowProgressFreq());
+		command.append(" PROCESS_CNT=").append(match.getMatchProcessCnt());
+		return command.toString();
+	}
+
     public static void main(String[] args) {
 
         final JFrame f = new RunMatchPanel(new PlMatch("Test Match","FIND DUPLICATE"));
