a9383ac46b6f8aa1fe6da4f5f6ac926fc758aab5
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-10-04 21:52:55 +0000

    finished match import function (except save and refersh Tree) working on viewStatsPanel of runMatchPanel

---
+++ src/ca/sqlpower/matchmaker/swingui/MatchStatsPanel.java
@@ -0,0 +1,29 @@
+package ca.sqlpower.matchmaker.swingui;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class MatchStatsPanel extends JPanel {
+
+	public MatchStatsPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MatchStatsPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MatchStatsPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MatchStatsPanel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/ca/sqlpower/matchmaker/swingui/RunMatchPanel.java
+++ src/ca/sqlpower/matchmaker/swingui/RunMatchPanel.java
@@ -11,7 +11,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
+import javax.sql.RowSet;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
@@ -23,9 +30,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
@@ -37,8 +49,13 @@ import org.hibernate.Transaction;
 
 import ca.sqlpower.architect.SQLDatabase;
 import ca.sqlpower.architect.swingui.ASUtils;
+import ca.sqlpower.architect.swingui.ArchitectPanelBuilder;
 import ca.sqlpower.architect.swingui.SaveDocument;
 import ca.sqlpower.architect.swingui.ASUtils.FileExtensionFilter;
+import ca.sqlpower.architect.swingui.table.DateTableCellRenderer;
+import ca.sqlpower.architect.swingui.table.NumberAndIntegerTableCellRenderer;
+import ca.sqlpower.architect.swingui.table.TableModelColumnAutofit;
+import ca.sqlpower.matchmaker.RowSetModel;
 import ca.sqlpower.matchmaker.hibernate.PlMatch;
 import ca.sqlpower.matchmaker.util.HibernateUtil;
 
@@ -47,6 +64,7 @@ import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.debug.FormDebugPanel;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
+import com.sun.rowset.CachedRowSetImpl;
 
 public class RunMatchPanel extends JFrame{
 
@@ -230,11 +248,162 @@ public class RunMatchPanel extends JFrame{
     	plMatch.setMatchProcessCnt(Long.valueOf(recordsToProcess.getText()));
     }
 
-    public class EngineRunner implements Runnable {
 
-		public void run() {
+
+    public RowSet getMatchStats(PlMatch match) throws SQLException {
+    	Connection con = null;
+    	Statement stmt = null;
+    	ResultSet rs =  null;
+    	try {
+    		con = HibernateUtil.primarySession().connection();
+    		StringBuffer sql = new StringBuffer();
+    		sql.append("SELECT TRANS_RUN_NO,START_DATE_TIME,ELAPSED_TIME");
+    		sql.append(",RUN_STATUS,NO_OF_REC_READ,NO_OF_REC_ADDED");
+    		sql.append(",NO_OF_REC_UPDATED,NO_OF_REC_TOTAL,NO_OF_REC_PROCESSED");
+    		sql.append(",RUN_NO,ROLLBACK_SEGMENT_NAME");
+    		sql.append(" FROM PL_STATS WHERE OBJECT_TYPE=? ");
+    		sql.append(" AND OBJECT_NAME=? ");
+    		sql.append(" ORDER BY TRANS_RUN_NO DESC, START_DATE_TIME DESC");
+    		PreparedStatement pstmt = con.prepareStatement(sql.toString());
+    		pstmt.setString(1, "MATCH");
+    		pstmt.setString(2, match.getMatchId());
+    		rs = pstmt.executeQuery();
+
+    		CachedRowSetImpl crset = new CachedRowSetImpl();
+    		crset.setReadOnly(true);
+    		crset.populate(rs);
+    		return crset;
+    	} finally {
+    		if ( rs != null )
+    			rs.close();
+    		if ( stmt != null )
+    			stmt.close();
+    		if (con != null)
+    			con.close();
+    	}
+    }
+
+    public RowSet getMatchGroupStats(PlMatch match, int runNo) throws SQLException {
+    	Connection con = null;
+    	PreparedStatement pstmt = null;
+    	ResultSet rs =  null;
+    	try {
+    		con = HibernateUtil.primarySession().connection();
+
+    		StringBuffer sql = new StringBuffer();
+    		sql.append("SELECT OBJECT_NAME,NO_OF_REC_ADDED, NO_OF_REC_UPDATED");
+    		sql.append(",NO_OF_REC_TOTAL, NO_OF_REC_READ,NO_OF_REC_PROCESSED");
+    		sql.append(" FROM PL_STATS WHERE OBJECT_TYPE=? and TRANS_RUN_NO=?");
+    		pstmt = con.prepareStatement(sql.toString());
+    		pstmt.setString(1, "MATCHGROUP");
+    		pstmt.setInt(2, runNo);
+    		rs = pstmt.executeQuery();
+    		CachedRowSetImpl crset = new CachedRowSetImpl();
+    		crset.setReadOnly(true);
+    		crset.populate(rs);
+    		rs.close();
+    		pstmt.close();
+
+    		sql = new StringBuffer();
+    		sql.append("SELECT GROUP_ID,MATCH_PERCENT FROM PL_MATCH_GROUP WHERE MATCH_ID=?");
+    		pstmt = con.prepareStatement(sql.toString());
+    		pstmt.setString(1, match.getMatchId());
+    		rs = pstmt.executeQuery();
+    		CachedRowSetImpl crset2 = new CachedRowSetImpl();
+    		crset2.setReadOnly(true);
+    		crset2.populate(rs);
+
+
+
+    		rs.close();
+    		pstmt.close();
+    		rs = null;
+    		pstmt = null;
+
+    		return crset;
+    	} finally {
+    		if ( rs != null )
+    			rs.close();
+    		if ( pstmt != null )
+    			pstmt.close();
+    		if (con != null)
+    			con.close();
+    	}
+    }
+
+    public class StatsTableMOdel extends RowSetModel {
+
+		public StatsTableMOdel(RowSet set) {
+			super(set);
 		}
 
+    }
+    private class ViewStats extends AbstractAction {
+    	private PlMatch match;
+
+		public ViewStats(PlMatch match) {
+    		super("View Status");
+    		this.match = match;
+    	}
+
+		public void actionPerformed(ActionEvent e) {
+			try {
+				RowSet rs = getMatchStats(match);
+				RowSetModel rsm = new RowSetModel(rs);
+
+
+
+				JTable table = new JTable(rsm);
+				JTableHeader header = table.getTableHeader();
+				header.getColumnModel().getColumn(0).setHeaderValue("Run #");
+				header.getColumnModel().getColumn(1).setHeaderValue("Start Time");
+				header.getColumnModel().getColumn(2).setHeaderValue("Elapsed(sec)");
+				header.getColumnModel().getColumn(3).setHeaderValue("Status");
+				header.getColumnModel().getColumn(4).setHeaderValue("Total Found");
+				header.getColumnModel().getColumn(5).setHeaderValue("Added");
+				header.getColumnModel().getColumn(6).setHeaderValue("Updated");
+				header.getColumnModel().getColumn(7).setHeaderValue("Total");
+				header.getColumnModel().getColumn(8).setHeaderValue("Overall(current)");
+				header.getColumnModel().getColumn(9).setHeaderValue("Job Run #");
+				header.getColumnModel().getColumn(10).setHeaderValue("Rollback Seg");
+
+				TableColumnModel cm = table.getColumnModel();
+		        for (int col = 0; col < cm.getColumnCount(); col++) {
+		            TableColumn tc = cm.getColumn(col);
+		            if ( Date.class.isAssignableFrom(table.getColumnClass(col)) ) {
+		            	tc.setCellRenderer(new DateTableCellRenderer());
+		            } else if ( Number.class.isAssignableFrom(table.getColumnClass(col)) ) {
+		            	tc.setCellRenderer(new NumberAndIntegerTableCellRenderer());
+		            }
+		        }
+		        TableModelColumnAutofit columnAutoFit =
+                    new TableModelColumnAutofit(rsm, table);
+                columnAutoFit.setTableHeader(header);
+                table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+				JScrollPane scroller = new JScrollPane(table);
+
+				JSplitPane splitter = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+				splitter.setDividerLocation(.5);
+				splitter.setLeftComponent(scroller);
+
+
+
+
+				JDialog d = new JDialog();
+				d.add(splitter);
+//				d.setPreferredSize(new Dimension(800,600));
+				d.pack();
+				d.setVisible(true);
+			} catch (SQLException e1) {
+				ASUtils.showExceptionDialog(
+						RunMatchPanel.this,
+						"SQL Error", e1);
+				e1.printStackTrace();
+			}
+
+
+		}
     }
 
     public class StreamGobbler extends Thread
@@ -271,20 +440,6 @@ public class RunMatchPanel extends JFrame{
 				e.printStackTrace();
 			}
     	}
-    }
-
-    private class ViewStats extends AbstractAction {
-    	private PlMatch match;
-
-		public ViewStats(PlMatch match) {
-    		super("View Status");
-    		this.match = match;
-    	}
-
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-
-		}
     }
 
     private class RunEngineAction extends AbstractAction {
@@ -441,9 +596,15 @@ public class RunMatchPanel extends JFrame{
 
     			Runtime rt = Runtime.getRuntime();
     			System.out.println("Execing " + cmd[0] + " " + cmd[1]);
-    			Process proc = rt.exec(cmd);
-    			int exitVal = proc.waitFor();
-    			System.out.println("ExitValue: " + exitVal);
+    			final Process proc = rt.exec(cmd);
+    			new Thread(new Runnable(){
+					public void run() {
+						try {
+							proc.waitFor();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}}).start();
     		} catch (Throwable t) {
     			t.printStackTrace();
     		}
@@ -468,7 +629,7 @@ public class RunMatchPanel extends JFrame{
 			applyChange();
 			final String cmd = createCommand(match);
 			final JDialog d = new JDialog();
-			d.setTitle("MatchMaker Command Line:");
+			d.setTitle("MatchMaker Engine Command Line:");
 
 			final DefaultStyledDocument cmdDoc = new DefaultStyledDocument();
 			SimpleAttributeSet att = new SimpleAttributeSet();
@@ -530,9 +691,9 @@ public class RunMatchPanel extends JFrame{
 
 			pb.add(bbBuilder.getPanel(), cc.xy(2,4));
 			d.add(pb.getPanel());
+			ArchitectPanelBuilder.makeJDialogCancellable(d,null);
 			d.pack();
 			d.setVisible(true);
-
 		}
 
     }
