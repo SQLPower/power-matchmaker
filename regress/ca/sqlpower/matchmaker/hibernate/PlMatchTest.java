96682e658a9e25dd7992c415df669cda08c20bc8
ian <ian@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-10-25 21:23:47 +0000

    Back to being a ValueObjectEqualsTest subclass (from TestCase).

--- regress/ca/sqlpower/matchmaker/hibernate/PlMatchTest.java
+++ regress/ca/sqlpower/matchmaker/hibernate/PlMatchTest.java
@@ -5,20 +5,16 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import junit.framework.TestCase;
+import com.diasparsoftware.util.junit.ValueObjectEqualsTest;
 
-
-//import com.diasparsoftware.util.junit.ValueObjectEqualsTest;
-
-public class PlMatchTest extends /*ValueObjectEqualsTest*/TestCase {
+public class PlMatchTest extends ValueObjectEqualsTest {
 
 	/** Since PlMatch,equals() ONLY looks at MatchID, not fields, we must use this... */
 	public static final String FAKE_MATCH_ID = "12345";
 
-	PlMatch target = new PlMatch();
-
 	public void testGetChildCount() {
 		Set<PlMatchGroup> plMatchGroups = new TreeSet<PlMatchGroup>();
+		PlMatch target = new PlMatch();
 
 		target.addAllPlMatchGroups(plMatchGroups);
 		assertEquals(0, target.getChildCount());
@@ -28,18 +24,21 @@ public class PlMatchTest extends /*ValueObjectEqualsTest*/TestCase {
 	}
 
 	public void testEqualsObject() {
+		PlMatch target = new PlMatch();
 		assertEquals(target, target);
 	}
 
-	
-	//@Override
+
+	@Override
 	protected Object createControlInstance() throws Exception {
+		PlMatch target = new PlMatch();
 		target.setMatchId(FAKE_MATCH_ID);
 		return target;
 	}
 
-	//@Override
+	@Override
 	protected Object createInstanceDiffersIn(String arg0) throws Exception {
+		PlMatch target = new PlMatch();
 		target.setMatchId(FAKE_MATCH_ID);
 		if ("matchId".equals(arg0)) {
 			target.setMatchId("foo");
@@ -47,7 +46,7 @@ public class PlMatchTest extends /*ValueObjectEqualsTest*/TestCase {
 		return target;
 	}
 
-	//@Override
+	@Override
 	protected List keyPropertyNames() {
 		return Arrays.asList(new String[] { "matchId" });
 	}
