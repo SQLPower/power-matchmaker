6e6a3ed5eed20b28726be7a886de9a5500006f5e
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-10-31 20:31:18 +0000

    added icons for matchmaker use icons on JTree

---
+++ trunk/src/ca/sqlpower/matchmaker/swingui/MatchMakerTreeCellRenderer.java
@@ -0,0 +1,32 @@
+package ca.sqlpower.matchmaker.swingui;
+
+import java.awt.Component;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+import ca.sqlpower.matchmaker.hibernate.PlMatch;
+import ca.sqlpower.matchmaker.hibernate.PlMatchGroup;
+
+public class MatchMakerTreeCellRenderer extends DefaultTreeCellRenderer {
+
+	final private Icon matchIcon = new ImageIcon(getClass().getResource("/icons/gears_16.png"));
+	final private Icon groupIcon = new ImageIcon(getClass().getResource("/icons/gear_16.png"));
+
+	public Component getTreeCellRendererComponent(JTree tree, Object value,
+			boolean selected, boolean expanded, boolean leaf, int row,
+			boolean hasFocus) {
+
+		super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+
+		if (value instanceof PlMatch) {
+			setIcon(matchIcon);
+		} else if (value instanceof PlMatchGroup) {
+			setIcon(groupIcon);
+		}
+		return this;
+	}
+
+}
