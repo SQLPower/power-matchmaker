695035937991aeeca41fae5b2eace0395de9f8be
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-03 19:42:12 +0000

    Initial attempt at methods of MatchMakerObject interface

--- trunk/src/ca/sqlpower/matchmaker/MatchMakerObject.java
+++ trunk/src/ca/sqlpower/matchmaker/MatchMakerObject.java
@@ -1,5 +1,42 @@
 package ca.sqlpower.matchmaker;
 
-public class MatchMakerObject {
+import java.util.List;
 
+import ca.sqlpower.matchmaker.event.MatchMakerListener;
+
+/**
+ * The interface for all of the match maker specific business objects
+ *
+ */
+public interface MatchMakerObject<T extends MatchMakerObject> extends Auditable {
+	
+	/**
+	 * Support for adding match maker event listeners
+	 */
+	void addMatchMakerListener(MatchMakerListener l);
+	
+	/**
+	 * Support for adding match maker event listeners
+	 */
+	void removeMatchMakerListener(MatchMakerListener l);
+	
+	/**
+	 * Returns the object's primary children
+	 */
+	List<T> getChildren();
+	
+	/**
+	 * Get the number of children
+	 */
+	int getChildCount();
+	
+	/**
+	 * Add a new child to this object
+	 */
+	void addChild(T child);
+	
+	/**
+	 * remove a child from this object
+	 */
+	void removeChild(T child);
 }
