738a6848506481da5b1538785742b0bb5225b153
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-06 17:50:45 +0000

    Created the setting objects for the match and the merge engines

---
+++ trunk/regress/ca/sqlpower/matchmaker/TestMatchSettings.java
@@ -0,0 +1,16 @@
+package ca.sqlpower.matchmaker;
+
+public class TestMatchSettings extends MatchMakerTestCase {
+
+	MatchSettings ms;
+	protected void setUp() throws Exception {
+		super.setUp();
+		ms = new MatchSettings("Test User");
+	}
+
+	@Override
+	protected MatchMakerObject<MatchMakerObject> getTarget() {
+		return ms;
+	}
+
+}
---
+++ trunk/src/ca/sqlpower/matchmaker/MatchSettings.java
@@ -0,0 +1,28 @@
+package ca.sqlpower.matchmaker;
+
+/**
+ * Settings that are specific to the Match engine
+ */
+public class MatchSettings extends MatchMakerSettings {
+	
+	public MatchSettings(String appUserName) {
+		super(appUserName);
+	}
+
+	/**
+	 * Breakup the match after each criteria group
+	 */
+	boolean breakUpMatch;
+
+	public boolean isBreakUpMatch() {
+		return breakUpMatch;
+	}
+
+	public void setBreakUpMatch(boolean breakUpMatch) {
+		if (this.breakUpMatch != breakUpMatch) {
+			boolean oldValue = this.breakUpMatch;
+			this.breakUpMatch = breakUpMatch;
+			getEventSupport().firePropertyChange("breakUpMatch", oldValue, breakUpMatch);
+		}
+	}
+}
