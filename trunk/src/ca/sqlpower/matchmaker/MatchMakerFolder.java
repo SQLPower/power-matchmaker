fb4b5e40ef7d93a0f09e494020e9b85eda3bb373
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-06 22:08:01 +0000

    Fixed type parameter and added name and description properties.

--- trunk/src/ca/sqlpower/matchmaker/MatchMakerFolder.java
+++ trunk/src/ca/sqlpower/matchmaker/MatchMakerFolder.java
@@ -1,15 +1,40 @@
 package ca.sqlpower.matchmaker;
 
+
+
 /** 
  * A container class desigend to hold match maker objects
  *	
  * FIXME implement class
  */
-public class MatchMakerFolder extends AbstractMatchMakerObject<MatchMakerObject> {
+public class MatchMakerFolder<C extends MatchMakerObject> extends AbstractMatchMakerObject<C> {
 
+	private String folderName;
+    private String folderDesc;
+    
 	public MatchMakerFolder(String appUserName) {
 		super(appUserName);
-		// TODO Auto-generated constructor stub
 	}
+
+	public String getFolderDesc() {
+		return folderDesc;
+	}
+
+	public void setFolderDesc(String folderDesc) {
+		String oldValue = this.folderDesc;
+		this.folderDesc = folderDesc;
+		getEventSupport().firePropertyChange("folderDesc", oldValue, folderDesc);
+	}
+
+	public String getFolderName() {
+		return folderName;
+	}
+
+	public void setFolderName(String folderName) {
+		String oldValue = this.folderName;
+		this.folderName = folderName;
+		getEventSupport().firePropertyChange("folderName", oldValue, folderName);
+	}
+
 
 }
