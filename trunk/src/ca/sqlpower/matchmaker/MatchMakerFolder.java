3674aa5bce22412350cd97d859d81a50794b5335
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-06 15:57:50 +0000

    Created a stub class for the folders to be used as children of match

---
+++ trunk/src/ca/sqlpower/matchmaker/MatchMakerFolder.java
@@ -0,0 +1,15 @@
+package ca.sqlpower.matchmaker;
+
+/** 
+ * A container class desigend to hold match maker objects
+ *	
+ * FIXME implement class
+ */
+public class MatchMakerFolder extends AbstractMatchMakerObject {
+
+	public MatchMakerFolder(String appUserName) {
+		super(appUserName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
