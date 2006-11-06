51b35773d53421340d6838a01427bcf8477e5f88
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-06 15:51:57 +0000

    Added a merge strategy object to use as a stub

---
+++ src/ca/sqlpower/matchmaker/MergeStrategy.java
@@ -0,0 +1,16 @@
+package ca.sqlpower.matchmaker;
+
+/**
+ * 
+ * Merge strategy handles a per table setup of the merge engine
+ * 
+ * FIXME Write the code
+ */
+public class MergeStrategy extends AbstractMatchMakerObject {
+
+	public MergeStrategy(String appUserName) {
+		super(appUserName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
