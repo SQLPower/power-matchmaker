b36a86bf8ccabe89aed6d845ca652296372f71ca
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-06 20:05:10 +0000

    Created a view spec stub

---
+++ trunk/src/ca/sqlpower/matchmaker/util/ViewSpec.java
@@ -0,0 +1,62 @@
+package ca.sqlpower.matchmaker.util;
+/**
+ * Specification for a view
+ *
+ * FIXME write me
+ */
+public class ViewSpec extends SQLQuery {
+	/** the view's name */
+	private String name;
+	/** The jdbc catalog containing the view */
+	private String catalog;
+	/** the jdbc schema containing the view */
+	private String schema;
+	
+	
+	private void create(){
+		
+	}
+	
+	private void drop(){
+		
+	}
+	
+	private void replace(){
+		
+	}
+	
+	private void verifyQuery(){
+		
+	}
+
+	public String getCatalog() {
+		return catalog;
+	}
+
+	public void setCatalog(String catalog) {
+		if (this.catalog != catalog) {
+			this.catalog = catalog;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (this.name != name) {
+			this.name = name;
+		}
+	}
+
+	public String getSchema() {
+		return schema;
+	}
+
+	public void setSchema(String schema) {
+		if (this.schema != schema) {
+			this.schema = schema;
+		}
+	}
+
+}
