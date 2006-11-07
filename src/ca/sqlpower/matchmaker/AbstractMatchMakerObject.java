82de02ee104e26c2790060b737e26bb7c3adce50
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-07 21:47:35 +0000

    fixed the constructor error: do not change last_update_user until change some properties or children added listener on itself for catch children change (change the auditing info) fire InsertChildEvent and Remove Child Event for addChild and removeChild

--- src/ca/sqlpower/matchmaker/AbstractMatchMakerObject.java
+++ src/ca/sqlpower/matchmaker/AbstractMatchMakerObject.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import ca.sqlpower.matchmaker.event.MatchMakerEvent;
 import ca.sqlpower.matchmaker.event.MatchMakerEventSupport;
 import ca.sqlpower.matchmaker.event.MatchMakerListener;
 
-public abstract class AbstractMatchMakerObject<C extends MatchMakerObject> implements MatchMakerObject<C> {
+/**
+ * The abstract class of MatchMakerObject, it has a listener listens to the change
+ * of children, properties and structure, any thing changed in the object will
+ * cause auditing information changes.
+ *
+ * @param <C>
+ */
+public abstract class AbstractMatchMakerObject<C extends MatchMakerObject>
+	implements MatchMakerObject<C> {
 
 	private MatchMakerEventSupport<MatchMakerObject,C> eventSupport =
 		new MatchMakerEventSupport<MatchMakerObject,C>(this);
@@ -15,21 +24,49 @@ public abstract class AbstractMatchMakerObject<C extends MatchMakerObject> imple
 	private String lastUpdateAppUser;
 	private String lastUpdateOsUser;
 	private Date lastUpdateDate;
+	private String appUserName;
 
-	
+
 	public AbstractMatchMakerObject(String appUserName) {
-		lastUpdateAppUser = appUserName;
+		this.appUserName = appUserName;
+		eventSupport.addMatchMakerListener(new MatchMakerListener<MatchMakerObject,C>(){
+
+			public void mmPropertyChanged(MatchMakerEvent<MatchMakerObject, C> evt) {
+				registerUpdate();
+			}
+
+			public void mmChildrenInserted(MatchMakerEvent<MatchMakerObject, C> evt) {
+				registerUpdate();
+			}
+
+			public void mmChildrenRemoved(MatchMakerEvent<MatchMakerObject, C> evt) {
+				registerUpdate();
+			}
+
+			public void mmStructureChanged(MatchMakerEvent<MatchMakerObject, C> evt) {
+				registerUpdate();
+			}});
 	}
 
+	/**
+	 * anyone who going to overwrite this method should fire the childrenInserted
+	 * event in the overwriten method
+	 * @param child
+	 */
 	public void addChild(C child) {
 		children.add(child);
+		int [] insertedIndices = {children.size()};
+		List<C> insertedChildren = new ArrayList<C>();
+		insertedChildren.add(child);
+		eventSupport.fireChildrenInserted("children",insertedIndices,insertedChildren);
 	}
 
 	public void addMatchMakerListener(MatchMakerListener<MatchMakerObject ,C> l) {
 		eventSupport.addMatchMakerListener(l);
 
 	}
-	
+
+
 	protected MatchMakerEventSupport<MatchMakerObject,C> getEventSupport() {
 		return eventSupport;
 	}
@@ -42,8 +79,17 @@ public abstract class AbstractMatchMakerObject<C extends MatchMakerObject> imple
 		return children;
 	}
 
+	/**
+	 * anyone who going to overwrite this method should fire the ChildrenRemoved
+	 * event in the overwriten method
+	 * @param child
+	 */
 	public void removeChild(C child) {
+		int [] removedIndices = {children.indexOf(child)};
+		List<C> removedChildren = new ArrayList<C>();
+		removedChildren.add(child);
 		children.remove(child);
+		eventSupport.fireChildrenRemoved("children",removedIndices,removedChildren);
 	}
 
 	public void removeMatchMakerListener(MatchMakerListener<MatchMakerObject ,C> l) {
@@ -62,6 +108,6 @@ public abstract class AbstractMatchMakerObject<C extends MatchMakerObject> imple
 	public void registerUpdate() {
 		lastUpdateDate = new Date(System.currentTimeMillis());
 		lastUpdateOsUser = System.getProperty("user.name");
-		// TODO: update app user
+		lastUpdateAppUser = appUserName;
 	}
 }
