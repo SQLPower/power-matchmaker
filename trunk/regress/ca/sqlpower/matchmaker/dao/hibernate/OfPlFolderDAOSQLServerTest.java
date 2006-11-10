d901a27f62dbf2cafefd7493f2228733f0dfe83c
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-10 17:12:04 +0000

    Created a test for sql server

---
+++ trunk/regress/ca/sqlpower/matchmaker/dao/hibernate/OfPlFolderDAOSQLServerTest.java
@@ -0,0 +1,27 @@
+package ca.sqlpower.matchmaker.dao.hibernate;
+
+import ca.sqlpower.matchmaker.MatchMakerObject;
+import ca.sqlpower.matchmaker.PlFolder;
+import ca.sqlpower.matchmaker.dao.PlFolderDAO;
+
+
+public class OfPlFolderDAOSQLServerTest extends AbstractDAOTestCase<PlFolder<MatchMakerObject>,PlFolderDAO> {
+	int count=0;
+	
+	@Override
+	public PlFolderDAO getDataAccessObject() {
+		return new PlFolderDAOHibernate(HibernateTestUtil.getSqlServerSessionFactory(),"Test User");
+	}
+
+	@Override
+	public PlFolder<MatchMakerObject> getNewObjectUnderTest() {
+		count++;
+		PlFolder<MatchMakerObject> plFolder = new PlFolder<MatchMakerObject>();
+		plFolder.setAppUserName("test user");
+		plFolder.setFolderName("test "+count);
+		return plFolder;
+	}
+
+	
+	
+}
