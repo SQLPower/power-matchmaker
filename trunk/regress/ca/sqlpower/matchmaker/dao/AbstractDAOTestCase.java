dca9b12e735b5138cc67dbe2fddb009f0f087d86
matthew <matthew@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-10 19:31:03 +0000

    Added test case to check if all the properties persist

--- trunk/regress/ca/sqlpower/matchmaker/dao/AbstractDAOTestCase.java
+++ trunk/regress/ca/sqlpower/matchmaker/dao/AbstractDAOTestCase.java
@@ -1,18 +1,25 @@
-package ca.sqlpower.matchmaker.dao.hibernate;
+package ca.sqlpower.matchmaker.dao;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import junit.framework.TestCase;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+
 import ca.sqlpower.matchmaker.MatchMakerObject;
 import ca.sqlpower.matchmaker.MatchMakerSession;
 import ca.sqlpower.matchmaker.TestingMatchMakerSession;
-import ca.sqlpower.matchmaker.dao.MatchMakerDAO;
 
 public abstract class AbstractDAOTestCase<T extends MatchMakerObject, D extends MatchMakerDAO> extends TestCase {
 
 	public MatchMakerSession session = new TestingMatchMakerSession();
 	
-	public void testFindAll(){
+	public void testFindAll() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException{
 		D dao = getDataAccessObject();
 		List<T> all = dao.findAll();
 		if (all.size() == 0) {
@@ -34,7 +41,7 @@ public abstract class AbstractDAOTestCase<T extends MatchMakerObject, D extends 
 		assertEquals("There are still some objects of type "+item1.getClass()+" left",0,dao.findAll().size());
 	}
 	
-	public void testSaveAndLoadInOneSession(){
+	public void testSaveAndLoadInOneSession() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException{
 		T item1 = getNewObjectUnderTest();
 		T item2 = getNewObjectUnderTest();
 		D dao = getDataAccessObject();
@@ -44,13 +51,32 @@ public abstract class AbstractDAOTestCase<T extends MatchMakerObject, D extends 
 		assertTrue("All should  be larger than 1",all.size()>1);
 		assertTrue("The "+item1.getClass() + " item1 was not in the list",all.contains(item1));
 		assertTrue("The "+item2.getClass() + " item2 was not in the list",all.contains(item2));
+		
+		T savedItem1 = all.get(all.indexOf(item1));
+		List<PropertyDescriptor> properties;
+		properties = Arrays.asList(PropertyUtils.getPropertyDescriptors(item1.getClass()));
+		
+		List<PropertyDescriptor> gettableProperties = new ArrayList<PropertyDescriptor>();
+		for (PropertyDescriptor d: properties){
+			if( d.getReadMethod() != null ) {
+				gettableProperties.add(d);
+			}
+		}
+		
+		List<String> nonPersistingProperties = getNonPersitingProperties();
+		for (PropertyDescriptor d: gettableProperties){
+			if (!nonPersistingProperties.contains(d.getName())) {
+				 Object old = BeanUtils.getSimpleProperty(item1, d.getName());
+				 Object newItem = BeanUtils.getSimpleProperty(savedItem1, d.getName());
+				 assertEquals("The property "+d.getName() +" was not persisted for object "+this.getClass(),old,newItem);
+			}
+		}
 	}
-	
 	
 	/**
 	 * Should return a new instance of an object that is being used by the 
 	 * DAO.  Each new object must be not equal to any of the previously 
-	 * created objects.
+	 * created objects.  Every gettable property must be set to a non-default value
 	 * 
 	 * @return a new test object 
 	 */
@@ -60,6 +86,13 @@ public abstract class AbstractDAOTestCase<T extends MatchMakerObject, D extends 
 	 * This should return the data access object that is being tested
 	 */
 	public abstract D getDataAccessObject();
-		
+	
+	/**
+	 * gets a list of strings that this object dosn't persist
+	 */
+	public List<String> getNonPersitingProperties(){
+		ArrayList<String> nonPersisting = new ArrayList<String>();
+		return nonPersisting;
+	}
 	
 }
--- trunk/regress/ca/sqlpower/matchmaker/dao/hibernate/PlFolderDAOOracleTest.java
+++ trunk/regress/ca/sqlpower/matchmaker/dao/hibernate/PlFolderDAOOracleTest.java
@@ -1,27 +1,12 @@
 package ca.sqlpower.matchmaker.dao.hibernate;
 
-import ca.sqlpower.matchmaker.MatchMakerObject;
-import ca.sqlpower.matchmaker.PlFolder;
+import ca.sqlpower.matchmaker.dao.AbstractPlFolderDAOTestCase;
 import ca.sqlpower.matchmaker.dao.PlFolderDAO;
 
 
-public class PlFolderDAOOracleTest extends AbstractDAOTestCase<PlFolder<MatchMakerObject>,PlFolderDAO> {
-	int count=0;
-	
+public class PlFolderDAOOracleTest extends AbstractPlFolderDAOTestCase {
 	@Override
 	public PlFolderDAO getDataAccessObject() {
 		return new PlFolderDAOHibernate(HibernateTestUtil.getOracleSessionFactory(),session);
 	}
-
-	@Override
-	public PlFolder<MatchMakerObject> getNewObjectUnderTest() {
-		count++;
-		PlFolder<MatchMakerObject> plFolder = new PlFolder<MatchMakerObject>();
-		plFolder.setSession(this.session);
-		plFolder.setFolderName("test "+count);
-		return plFolder;
-	}
-
-	
-	
 }
