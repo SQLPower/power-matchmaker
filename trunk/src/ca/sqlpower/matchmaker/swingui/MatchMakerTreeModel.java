cda10d3e1ba22a8173b750be8e7515ff99f6afb7
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-14 16:13:43 +0000

    added listener to the new inserted children. fixed the unit test problem: most last event sources are mmo not folder or current folder

--- trunk/src/ca/sqlpower/matchmaker/swingui/MatchMakerTreeModel.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/MatchMakerTreeModel.java
@@ -17,16 +17,16 @@ import ca.sqlpower.matchmaker.PlFolder;
 import ca.sqlpower.matchmaker.event.MatchMakerEvent;
 import ca.sqlpower.matchmaker.event.MatchMakerListener;
 
-public class MatchMakerTreeModel
-	implements TreeModel {
+public class MatchMakerTreeModel implements TreeModel {
 
-    private static final Logger logger = Logger.getLogger(MatchMakerTreeModel.class);
+	private static final Logger logger = Logger
+			.getLogger(MatchMakerTreeModel.class);
 
-    private static class MMTreeNode<C extends MatchMakerObject>
-    	extends AbstractMatchMakerObject<MMTreeNode,C> {
+	private static class MMTreeNode<C extends MatchMakerObject> extends
+			AbstractMatchMakerObject<MMTreeNode, C> {
 
-    	public MMTreeNode(String name) {
-    		setName(name);
+		public MMTreeNode(String name) {
+			setName(name);
 		}
 
 		@Override
@@ -39,10 +39,16 @@ public class MatchMakerTreeModel
 			return System.identityHashCode(this);
 		}
 
-    }
-	private final MMTreeNode<MMTreeNode> root = new MMTreeNode<MMTreeNode>("root");
-	private final MMTreeNode<PlFolder> current = new MMTreeNode<PlFolder>("Current Match/Merge Information");
-	private final MMTreeNode<PlFolder> backup = new MMTreeNode<PlFolder>("Backup Match/Merge Information");
+	}
+
+	private final MMTreeNode<MMTreeNode> root = new MMTreeNode<MMTreeNode>(
+			"root");
+
+	private final MMTreeNode<PlFolder> current = new MMTreeNode<PlFolder>(
+			"Current Match/Merge Information");
+
+	private final MMTreeNode<PlFolder> backup = new MMTreeNode<PlFolder>(
+			"Backup Match/Merge Information");
 
 	private TreeModelEventAdapter listener = new TreeModelEventAdapter();
 
@@ -50,22 +56,22 @@ public class MatchMakerTreeModel
 		root.addChild(current);
 		root.addChild(backup);
 
-		for ( PlFolder f : folders ) {
+		for (PlFolder f : folders) {
 			current.addChild(f);
 		}
 		MatchMakerUtils.listenToHierarchy(listener, root);
 	}
 
 	public Object getChild(Object parent, int index) {
-		return ((MatchMakerObject<?,?>)parent).getChildren().get(index);
+		return ((MatchMakerObject<?, ?>) parent).getChildren().get(index);
 	}
 
 	public int getChildCount(Object parent) {
-		return ((MatchMakerObject<?,?>) parent).getChildren().size();
+		return ((MatchMakerObject<?, ?>) parent).getChildren().size();
 	}
 
 	public int getIndexOfChild(Object parent, Object child) {
-		return ((MatchMakerObject<?,?>) parent).getChildren().indexOf(child);
+		return ((MatchMakerObject<?, ?>) parent).getChildren().indexOf(child);
 	}
 
 	public Object getRoot() {
@@ -73,17 +79,15 @@ public class MatchMakerTreeModel
 	}
 
 	public boolean isLeaf(Object node) {
-		return !((MatchMakerObject<?,?>)node).allowsChildren();
+		return !((MatchMakerObject<?, ?>) node).allowsChildren();
 	}
-
 
 	public void valueForPathChanged(TreePath path, Object newValue) {
-		throw new UnsupportedOperationException("Value for path change " +
-				"unsupported in the match maker tree");
+		throw new UnsupportedOperationException("Value for path change "
+				+ "unsupported in the match maker tree");
 	}
 
-	protected LinkedList<TreeModelListener> treeModelListeners =
-		new LinkedList<TreeModelListener>();
+	protected LinkedList<TreeModelListener> treeModelListeners = new LinkedList<TreeModelListener>();
 
 	public void addTreeModelListener(TreeModelListener l) {
 		treeModelListeners.add(l);
@@ -94,78 +98,86 @@ public class MatchMakerTreeModel
 	}
 
 	protected void fireTreeNodesInserted(TreeModelEvent e) {
-        logger.debug("Firing treeNodesInserted event "+
-        		e+" to "+treeModelListeners.size()+" listeners...");
-		for (int i= treeModelListeners.size()-1; i >=0; i--){
+		logger.debug("Firing treeNodesInserted event " + e + " to "
+				+ treeModelListeners.size() + " listeners...");
+		for (int i = treeModelListeners.size() - 1; i >= 0; i--) {
 			treeModelListeners.get(i).treeNodesInserted(e);
 		}
-        logger.debug("done");
+		logger.debug("done");
 	}
 
 	protected void fireTreeNodesRemoved(TreeModelEvent e) {
-        logger.debug("Firing treeNodesRemoved event "+
-        		e+" to "+treeModelListeners.size()+" listeners...");
-		for (int i= treeModelListeners.size()-1; i >=0; i--){
+		logger.debug("Firing treeNodesRemoved event " + e + " to "
+				+ treeModelListeners.size() + " listeners...");
+		for (int i = treeModelListeners.size() - 1; i >= 0; i--) {
 			treeModelListeners.get(i).treeNodesRemoved(e);
 		}
-        logger.debug("done");
+		logger.debug("done");
 	}
 
 	protected void fireTreeNodesChanged(TreeModelEvent e) {
-        logger.debug("Firing treeNodesChanged event "+
-        		e+" to "+treeModelListeners.size()+" listeners...");
-		for (int i= treeModelListeners.size()-1; i >=0; i--){
-            logger.debug("Notifing "+treeModelListeners.get(i));
+		logger.debug("Firing treeNodesChanged event " + e + " to "
+				+ treeModelListeners.size() + " listeners...");
+		for (int i = treeModelListeners.size() - 1; i >= 0; i--) {
+			logger.debug("Notifing " + treeModelListeners.get(i));
 			treeModelListeners.get(i).treeNodesChanged(e);
 		}
-        logger.debug("done");
+		logger.debug("done");
 	}
 
 	protected void fireTreeStructureChanged(TreeModelEvent e) {
-        logger.debug("Firing treeStructureChanged event "+
-        		e+" to "+treeModelListeners.size()+" listeners...");
-		for (int i= treeModelListeners.size()-1; i >=0; i--){
+		logger.debug("Firing treeStructureChanged event " + e + " to "
+				+ treeModelListeners.size() + " listeners...");
+		for (int i = treeModelListeners.size() - 1; i >= 0; i--) {
 			treeModelListeners.get(i).treeStructureChanged(e);
 		}
-        logger.debug("done");
+		logger.debug("done");
 	}
 
 	public void refresh() {
-		fireTreeNodesChanged(new TreeModelEvent(root,new TreePath(root)));
+		fireTreeNodesChanged(new TreeModelEvent(root, new TreePath(root)));
 	}
 
-	private class TreeModelEventAdapter<T extends MatchMakerObject, C extends MatchMakerObject> implements MatchMakerListener<T, C> {
+	private class TreeModelEventAdapter<T extends MatchMakerObject, C extends MatchMakerObject>
+			implements MatchMakerListener<T, C> {
 		public void mmPropertyChanged(MatchMakerEvent<T, C> evt) {
 			TreePath paths = getPathForNode(evt.getSource());
-			TreeModelEvent e = new TreeModelEvent(evt.getSource(),paths);
+			TreeModelEvent e = new TreeModelEvent(evt.getSource(), paths);
 			fireTreeNodesChanged(e);
 		}
 
 		public void mmChildrenInserted(MatchMakerEvent<T, C> evt) {
 			TreePath paths = getPathForNode(evt.getSource());
-			TreeModelEvent e = new TreeModelEvent(evt.getSource(),paths,
-					evt.getChangeIndices(),evt.getChildren().toArray());
+			TreeModelEvent e = new TreeModelEvent(evt.getSource(), paths, evt
+					.getChangeIndices(), evt.getChildren().toArray());
 			fireTreeNodesInserted(e);
+			/*
+			 * not sure where to put these code, but we need to add listener to the
+			 * new children on the tree
+			 */
+			for ( MatchMakerObject o : evt.getChildren() ) {
+				MatchMakerUtils.listenToHierarchy(listener,o);
+			}
 		}
 
 		public void mmChildrenRemoved(MatchMakerEvent<T, C> evt) {
 			TreePath paths = getPathForNode(evt.getSource());
-			TreeModelEvent e = new TreeModelEvent(evt.getSource(),paths,
-					evt.getChangeIndices(),evt.getChildren().toArray());
+			TreeModelEvent e = new TreeModelEvent(evt.getSource(), paths, evt
+					.getChangeIndices(), evt.getChildren().toArray());
 			fireTreeNodesRemoved(e);
 		}
 
 		public void mmStructureChanged(MatchMakerEvent<T, C> evt) {
 			TreePath paths = getPathForNode(evt.getSource());
-			TreeModelEvent e = new TreeModelEvent(evt.getSource(),paths);
+			TreeModelEvent e = new TreeModelEvent(evt.getSource(), paths);
 			fireTreeStructureChanged(e);
 		}
 	}
 
-	private TreePath getPathForNode(MatchMakerObject<?,?> source) {
+	private TreePath getPathForNode(MatchMakerObject<?, ?> source) {
 		List<MatchMakerObject> path = new LinkedList<MatchMakerObject>();
-		while ( source != null ) {
-			path.add(0,source);
+		while (source != null) {
+			path.add(0, source);
 			source = source.getParent();
 		}
 		return new TreePath(path.toArray());
