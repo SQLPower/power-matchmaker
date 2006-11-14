48ab1cbe379c5e3b531a175beade4a361dcd2c9f
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-14 16:13:43 +0000

    added listener to the new inserted children. fixed the unit test problem: most last event sources are mmo not folder or current folder

--- regress/ca/sqlpower/matchmaker/swingui/MatchMakerTreeModelTest.java
+++ regress/ca/sqlpower/matchmaker/swingui/MatchMakerTreeModelTest.java
@@ -13,14 +13,15 @@ public class MatchMakerTreeModelTest extends TestCase {
 	MatchMakerTreeEventCounter counter;
 	MatchMakerTreeModel treeModel;
 	private MatchMakerObject<MatchMakerObject, PlFolder> currentFoldersNode;
-	private final PlFolder<TestingAbstractMatchMakerObject> folder = new PlFolder<TestingAbstractMatchMakerObject>("Test Folder");
-	private final TestingAbstractMatchMakerObject mmo = new TestingAbstractMatchMakerObject();
+	private final PlFolder<TestingAbstractMatchMakerObject> folder =
+		new PlFolder<TestingAbstractMatchMakerObject>("Test Folder");
+	private final TestingAbstractMatchMakerObject mmo =
+		new TestingAbstractMatchMakerObject();
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		counter = new MatchMakerTreeEventCounter();
 		treeModel = new MatchMakerTreeModel(new ArrayList<PlFolder>());
-		treeModel.addTreeModelListener(counter);
 		currentFoldersNode = (MatchMakerObject<MatchMakerObject, PlFolder>) treeModel.getChild(treeModel.getRoot(), 0);
 		currentFoldersNode.addChild(folder);
 	}
@@ -33,33 +34,38 @@ public class MatchMakerTreeModelTest extends TestCase {
 				1, counter.getChildrenInsertedCount());
 		assertEquals("total event count should be 1",
 				1, counter.getAllEventCounts());
-		assertEquals("Last event source should be current folder",
-				currentFoldersNode,counter.getLastEvt().getSource());
+		assertEquals("Last event source should be folder",
+				folder,counter.getLastEvt().getSource());
+
+
 	}
 
 	public void testTreeNodeRemoveEvent() {
 
 		folder.addChild(mmo);
 		treeModel.addTreeModelListener(counter);
-		mmo.getEventSupport().fireChildrenRemoved("property name",new int[]{0}, Arrays.asList(new MatchMakerObject[] {mmo}));
+
+		mmo.getEventSupport().fireChildrenRemoved("property name",
+				new int[]{0}, Arrays.asList(new MatchMakerObject[] {mmo}));
 		assertEquals("remove event count should be 1",
 				1, counter.getChildrenRemovedCount());
 		assertEquals("total event count should be 1",
 				1, counter.getAllEventCounts());
-		assertEquals("Last event source should be current folder",
-				folder,counter.getLastEvt().getSource());
+		assertEquals("Last event source should be mmo",
+				mmo,counter.getLastEvt().getSource());
 	}
 
 	public void testTreeNodeChangeEvent() {
 
 		folder.addChild(mmo);
 		treeModel.addTreeModelListener(counter);
-		mmo.getEventSupport().firePropertyChange("property name","old value","new value");
+		mmo.getEventSupport().firePropertyChange("property name",
+				"old value","new value");
 		assertEquals("property change event count should be 1",
 				1, counter.getPropertyChangedCount());
 		assertEquals("total event count should be 1",
 				1, counter.getAllEventCounts());
-		assertEquals("Last event source should be current folder",
+		assertEquals("Last event source should be mmo",
 				mmo,counter.getLastEvt().getSource());
 	}
 
@@ -79,7 +85,7 @@ public class MatchMakerTreeModelTest extends TestCase {
 				1, counter.getStructureChangedCount());
 		assertEquals("total event count should be 1",
 				1, counter.getAllEventCounts());
-		assertEquals("Last event source should be current folder",
-				currentFoldersNode,counter.getLastEvt().getSource());
+		assertEquals("Last event source should be mmo",
+				mmo,counter.getLastEvt().getSource());
 	}
 }
