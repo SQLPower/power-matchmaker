d860c15c352277240db4b42504af6a7cfb55c021
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-16 23:14:31 +0000

    -added MatchMakerObjectCellRenderer, this renderer can be used by any MatchMakerObjects.  Got rid of FolderComboBoxCellRenderer since PlFolder can use this class now.

--- src/ca/sqlpower/matchmaker/swingui/MatchMakerObjectComboBoxCellRenderer.java
+++ src/ca/sqlpower/matchmaker/swingui/MatchMakerObjectComboBoxCellRenderer.java
@@ -7,17 +7,17 @@ import javax.swing.JList;
 
 import org.apache.log4j.Logger;
 
-import ca.sqlpower.matchmaker.PlFolder;
+import ca.sqlpower.matchmaker.MatchMakerObject;
 
-public class FolderComboBoxCellRenderer extends DefaultListCellRenderer {
-	Logger logger = Logger.getLogger(FolderComboBoxCellRenderer.class);
+public class MatchMakerObjectComboBoxCellRenderer extends DefaultListCellRenderer {
+	Logger logger = Logger.getLogger(MatchMakerObjectComboBoxCellRenderer.class);
 
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
 
 		super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 		if ( value != null ) {
-			setText(((PlFolder)value).getName());
+			setText(((MatchMakerObject)value).getName());
 		}
 		return this;
 	}
