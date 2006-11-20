003b8913d080e8c98f7f874f08c41a9d809ed2c2
ian <ian@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-20 22:10:19 +0000

    DummyAction is now only used inside MatchMakerSwingSession, so move it in there until we get rid of it altogether.

--- trunk/src/ca/sqlpower/matchmaker/swingui/MatchMakerSwingSession.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/MatchMakerSwingSession.java
@@ -650,4 +650,28 @@ public class MatchMakerSwingSession implements MatchMakerSession {
 		return sessionImpl.countMatchByName(name);
 	}
 
+	/**
+	 * XXX This is for actions that aren't written yet.
+	 * It will be removed when the application is completed;
+	 * only used in about half a dozen places now...
+	 */
+	private class DummyAction extends AbstractAction {
+
+		private String label;
+		private JFrame parent;
+
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(parent,
+				String.format("The %s action is not yet implemented", label),
+				"Apologies",
+				JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		public DummyAction(JFrame parent, String label) {
+			super(label);
+			this.label = label;
+			this.parent = parent;
+		}
+
+	}
 }
