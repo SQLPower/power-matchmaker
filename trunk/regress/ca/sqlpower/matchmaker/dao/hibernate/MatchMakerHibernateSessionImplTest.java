d3d8903e9889ebe9b8f25f10b700d000af1276b9
fuerth <fuerth@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-11-22 22:30:52 +0000

    Just some commented-out tests that should be run, but can't until a refactoring takes place.

---
+++ trunk/regress/ca/sqlpower/matchmaker/dao/hibernate/MatchMakerHibernateSessionImplTest.java
@@ -0,0 +1,35 @@
+package ca.sqlpower.matchmaker.dao.hibernate;
+
+import junit.framework.TestCase;
+
+public class MatchMakerHibernateSessionImplTest extends TestCase {
+
+    // FIXME all tests disabled until the HibernateSession becomes testable
+    
+//    /**
+//     * We're testing this object.
+//     */
+//    private MatchMakerHibernateSessionImpl session;
+//    
+//    @Override
+//    protected void setUp() throws Exception {
+//        ArchitectDataSource ds = new ArchitectDataSource();
+//        ds.setDriverClass("ca.sqlpower.architect.MockJDBCDriver");
+//        ds.setUrl("jdbc:mock:dbmd.catalogTerm=Catalog&dbmd.schemaTerm=Schema&catalogs=farm&schemas.farm=cow&tables.farm.cow=moo");
+//        ds.setUser("n/a");
+//        ds.setPass("n/a");
+//        session = new MatchMakerHibernateSessionImpl(null, ds);
+//    }
+//    
+//    public void testAddListenerAndFireWarning() {
+//        class MyWarningListener implements WarningListener {
+//            private int warningCount;
+//            public void handleWarning(String message) { warningCount++; }
+//        }
+//        MyWarningListener l = new MyWarningListener();
+//        session.addWarningListener(l);
+//        assertEquals(0, l.warningCount);
+//        session.handleWarning("cow");
+//        assertEquals(1, l.warningCount);
+//    }
+}
