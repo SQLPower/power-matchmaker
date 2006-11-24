546a1a1a09d54ec666a9d25ffaa9846252638ec9
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2006-11-24 02:35:30 +0000

    Refactored the test cases that check if cascading match group and criteria loads work properly, unifying the SQL Server and Oracle tests so both platforms have to pass the same set of assertions.
    The new abstract Match DAO test defines three abstract methods in support of this: one for inserting a record into each of PL_MATCH, PL_MATCH_GROUP, and PL_MATCH_CRITERIA.

--- regress/ca/sqlpower/matchmaker/dao/AbstractPlMatchDAOTestCase.java
+++ regress/ca/sqlpower/matchmaker/dao/AbstractPlMatchDAOTestCase.java
@@ -1,13 +1,12 @@
 package ca.sqlpower.matchmaker.dao;
 
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.List;
 
 import ca.sqlpower.matchmaker.Match;
 import ca.sqlpower.matchmaker.MatchMakerCriteria;
 import ca.sqlpower.matchmaker.MatchMakerCriteriaGroup;
+import ca.sqlpower.matchmaker.dao.hibernate.TestingMatchMakerHibernateSession;
 
 public abstract class AbstractPlMatchDAOTestCase extends AbstractDAOTestCase<Match,MatchDAO>  {
 
@@ -55,39 +54,82 @@ public abstract class AbstractPlMatchDAOTestCase extends AbstractDAOTestCase<Mat
 		return nonPersistingProperties;
 	}
 
+    /**
+     * Inserts data directly into the tables, then uses the DAO to retrieve the
+     * objects those rows represent, and checks that the objects are the ones we
+     * inserted.  Also implicitly checks that the descdendants are fetched eagerly
+     * (and don't depend on the hibernate session staying open).
+     */
     public void testIfChildrenLoadWorks() throws Exception {
         final long time = System.currentTimeMillis();
         final String matchName = "match_"+time;
-        Connection con = null;
-        Statement stmt = null;
-        try {
-            con = getSession().getConnection();
-            stmt = con.createStatement();
-            stmt.executeUpdate(
-                    "INSERT INTO pl_match (match_oid, match_id, match_type) " +
-                    "VALUES ("+time+", '"+matchName+"', '"+Match.MatchType.FIND_DUPES+"')");
-            stmt.executeUpdate(
-                    "INSERT INTO pl_match_group (group_oid, match_oid, group_id) " +
-                    "VALUES ("+time+", "+time+", 'group_"+time+"')");
-            stmt.executeUpdate(
-                    "INSERT INTO pl_match_criteria (group_oid, match_criteria_oid, column_name, last_update_user) " +
-                    "VALUES ("+time+", "+time+", 'fake_column_"+time+"', 'test_"+time+"')");
-        } finally {
-            try { stmt.close(); } catch (Exception e) { System.err.println("Couldn't close statement"); e.printStackTrace(); }
-            // connection didn't come from a pool so we can't close it
-        }
+        final long matchOid = insertSampleMatchData(matchName);
+        final long groupOid = insertSampleMatchCriteriaGroupData(matchOid, "group_"+time);
+        insertSampleMatchCriteriaData(groupOid, "test_crit_"+time);
         
         Match match = getDataAccessObject().findByName(matchName);
         
-        // this could fail if the DAO doesn't cascade the retrieval properly
-        List<MatchMakerCriteriaGroup> groups = match.getMatchCriteriaGroups();
-        assertEquals("There is only one criteria group", 1, groups.size());
-        MatchMakerCriteriaGroup group = groups.get(0);
-        assertEquals("Wrong Group name", "group_"+time, group.getName());
-        
-        List<MatchMakerCriteria> crits = group.getChildren();
-        assertEquals("There is only one set of column criteria", 1, crits.size());
-        MatchMakerCriteria crit = crits.get(0);
-        assertEquals("Wrong criteria last update user", "test_"+time, crit.getLastUpdateAppUser());
+        try {
+            ((TestingMatchMakerHibernateSession) getSession()).setConnectionDisabled(true);
+            List<MatchMakerCriteriaGroup> groups = match.getMatchCriteriaGroups();
+            assertEquals("There should be one criteria group",
+                    1, groups.size());
+
+            MatchMakerCriteriaGroup group = groups.get(0);
+            assertEquals("Wrong Group name",
+                    "group_"+time, group.getName());
+
+            List<MatchMakerCriteria> crits = group.getChildren();
+            assertEquals("There is only one set of column criteria",
+                    1, crits.size());
+
+            MatchMakerCriteria crit = crits.get(0);
+            assertEquals("Wrong criteria last update user",
+                    "test_crit_"+time, crit.getLastUpdateAppUser());
+        } finally {
+            ((TestingMatchMakerHibernateSession) getSession()).setConnectionDisabled(false);
+        }
+
     }
+
+    
+    /**
+     * Inserts a sample entry in PL_MATCH, and returns its OID.  The match will
+     * have its name (MATCH_ID) set to the given string value.  This is useful for
+     * verifying that you are retrieving the same record that this method inserted.
+     * <p>
+     * This method is abstract because the OID columns have to be handled
+     * differently on different database platforms.
+     * 
+     * @return The MATCH_OID value of the new match that was inserted.
+     */
+    protected abstract long insertSampleMatchData(
+            String matchName) throws Exception;
+    
+    /**
+     * Inserts a sample entry in PL_MATCH_GROUP, and returns its OID.  The group will
+     * its name (GROUP_ID) set to the given string value.  This is useful for
+     * verifying that you are retrieving the same record that this method inserted.
+     * <p>
+     * This method is abstract because the OID columns have to be handled
+     * differently on different database platforms.
+     * 
+     * @return The GROUP_OID value of the new match group that was inserted.
+     */
+    protected abstract long insertSampleMatchCriteriaGroupData(
+            long parentMatchOid, String groupName) throws Exception;
+
+    /**
+     * Inserts a sample entry in PL_MATCH_CRITERIA, and returns its OID.  The criteria will
+     * have its LAST_UPDATE_USER set to the given string value (it doesn't use the name
+     * because criteria groups don't have names).  This is useful for
+     * verifying that you are retrieving the same record that this method inserted.
+     * <p>
+     * This method is abstract because the OID columns have to be handled
+     * differently on different database platforms.
+     * 
+     * @return The GROUP_OID value of the new match group that was inserted.
+     */
+    protected abstract long insertSampleMatchCriteriaData(
+            long parentGroupOid, String lastUpdateUser) throws Exception;
 }
--- regress/ca/sqlpower/matchmaker/dao/hibernate/MatchDAOOracleTest.java
+++ regress/ca/sqlpower/matchmaker/dao/hibernate/MatchDAOOracleTest.java
@@ -1,6 +1,9 @@
 
 package ca.sqlpower.matchmaker.dao.hibernate;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 import ca.sqlpower.matchmaker.Match;
 import ca.sqlpower.matchmaker.dao.AbstractPlMatchDAOTestCase;
 import ca.sqlpower.matchmaker.dao.MatchDAO;
@@ -33,4 +36,60 @@ public class MatchDAOOracleTest extends AbstractPlMatchDAOTestCase {
     public void testMergeSettingsNotNullByDefault() {
         assertNotNull(match.getMergeSettings());
     }
+
+    @Override
+    protected long insertSampleMatchData(String matchName) throws Exception {
+        final long time = System.currentTimeMillis();
+        Connection con = null;
+        Statement stmt = null;
+        try {
+            con = getSession().getConnection();
+            stmt = con.createStatement();
+            stmt.executeUpdate(
+                    "INSERT INTO pl_match (match_oid, match_id, match_type) " +
+                    "VALUES ("+time+", '"+matchName+"', '"+Match.MatchType.FIND_DUPES+"')");
+        } finally {
+            try { stmt.close(); } catch (Exception e) { System.err.println("Couldn't close statement"); e.printStackTrace(); }
+            // connection didn't come from a pool so we can't close it
+        }
+        return time;
+    }
+
+    @Override
+    protected long insertSampleMatchCriteriaData(long parentGroupOid, String lastUpdateUser) throws Exception {
+        final long time = System.currentTimeMillis();
+        Connection con = null;
+        Statement stmt = null;
+        try {
+            con = getSession().getConnection();
+            stmt = con.createStatement();
+            stmt.executeUpdate(
+                    "INSERT INTO pl_match_criteria (group_oid, match_criteria_oid, column_name, last_update_user) " +
+                    "VALUES ("+parentGroupOid+", "+time+", 'fake_column_"+time+"', '"+lastUpdateUser+"')");
+        } finally {
+            try { stmt.close(); } catch (Exception e) { System.err.println("Couldn't close statement"); e.printStackTrace(); }
+            // connection didn't come from a pool so we can't close it
+        }
+        return time;
+    }
+
+    @Override
+    protected long insertSampleMatchCriteriaGroupData(long parentMatchOid, String groupName) throws Exception {
+        final long time = System.currentTimeMillis();
+        Connection con = null;
+        Statement stmt = null;
+        try {
+            con = getSession().getConnection();
+            stmt = con.createStatement();
+            stmt.executeUpdate(
+                    "INSERT INTO pl_match_group (group_oid, match_oid, group_id) " +
+                    "VALUES ("+time+", "+parentMatchOid+", '"+groupName+"')");
+        } finally {
+            try { stmt.close(); } catch (Exception e) { System.err.println("Couldn't close statement"); e.printStackTrace(); }
+            // connection didn't come from a pool so we can't close it
+        }
+        return time;
+    }
+    
+    
 }
