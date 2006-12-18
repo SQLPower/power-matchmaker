03a8e5dc4937dcb1685f802505b37043a48cf82d
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2006-12-18 22:51:50 +0000

    Removed pre-GraphModel graph building blocks that are not used

--- src/ca/sqlpower/matchmaker/swingui/graphViewer/DefaultEdgeFactory.java
+++
@@ -1,9 +0,0 @@
-package ca.sqlpower.matchmaker.swingui.graphViewer;
-
-public class DefaultEdgeFactory implements EdgeFactory {
-
-	public Diedge createEdge(Node head, Node tail) {
-		return new DefaultEdge(head,tail);
-	}
-
-}
--- src/ca/sqlpower/matchmaker/swingui/graphViewer/event/GraphComponentListener.java
+++
@@ -1,9 +0,0 @@
-package ca.sqlpower.matchmaker.swingui.graphViewer.event;
-
-
-public interface GraphComponentListener{
-	void gcPropertyChanged(GraphComponentEvent evt);
-	void gcEdgeDirectionSwap(GraphComponentEvent evt);
-	void gcEdgeCut(GraphComponentEvent evt);
-	void gcNewEdgeAddedToNode(GraphComponentEvent evt);
-}
