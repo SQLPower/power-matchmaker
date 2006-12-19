8e66b0dc5178111d1cd112a5a8d7d42bfd60c684
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2006-12-19 18:03:25 +0000

    -Changed from dialog to EditorPane API -Modified layout to include a panel of source table record viewers -Track the selected item in the graph and show the data associated with all the reachable records

--- src/ca/sqlpower/matchmaker/swingui/MatchResultVisualizer.java
+++ src/ca/sqlpower/matchmaker/swingui/MatchResultVisualizer.java
@@ -3,13 +3,14 @@ package ca.sqlpower.matchmaker.swingui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,12 +18,10 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
+import javax.swing.JSplitPane;
 
 import org.apache.log4j.Logger;
 
@@ -32,40 +31,77 @@ import ca.sqlpower.matchmaker.Match;
 import ca.sqlpower.matchmaker.MatchPool;
 import ca.sqlpower.matchmaker.PotentialMatchRecord;
 import ca.sqlpower.matchmaker.SourceTableRecord;
+import ca.sqlpower.matchmaker.graph.BreadthFirstSearch;
 import ca.sqlpower.matchmaker.graph.ConnectedComponentFinder;
 import ca.sqlpower.matchmaker.graph.GraphModel;
 import ca.sqlpower.matchmaker.graph.MatchPoolDotExport;
 import ca.sqlpower.matchmaker.graph.MatchPoolGraphModel;
-import ca.sqlpower.matchmaker.swingui.graphViewer.DefaultGraphLayoutCache;
-import ca.sqlpower.matchmaker.swingui.graphViewer.GraphLayoutCache;
 import ca.sqlpower.matchmaker.swingui.graphViewer.GraphNodeRenderer;
+import ca.sqlpower.matchmaker.swingui.graphViewer.GraphSelectionListener;
 import ca.sqlpower.matchmaker.swingui.graphViewer.GraphViewer;
+import ca.sqlpower.matchmaker.util.MatchMakerQFAFactory;
 
 /**
  * The MatchResultVisualizer produces graphical representations of the matches
  * in a Match Result Table.
  */
-public class MatchResultVisualizer {
+public class MatchResultVisualizer implements EditorPane {
     
     private static final Logger logger = Logger.getLogger(MatchResultVisualizer.class);
 
     /**
-     * This action just calls the {@link #refreshGraph()} method.
+     * Pops up a save dialog and saves the match pool to the chosen DOT file.
      */
     private final Action exportDotFileAction = new AbstractAction("Export as Dot file") {
         public void actionPerformed(ActionEvent e) {
             try {
-                refreshGraph();
+                File dotFile = new File(
+                        System.getProperty("user.home"), "matchmaker_graph_"+match.getName()+".dot");
+                JFileChooser fc = new JFileChooser(dotFile);
+                int choice = fc.showSaveDialog(panel);
+                if (choice == JFileChooser.APPROVE_OPTION) {
+                    MatchPoolDotExport exporter = new MatchPoolDotExport(match);
+                    exporter.setDotFile(fc.getSelectedFile());
+                    exporter.exportDotFile();
+                }
             } catch (Exception ex) {
-                ASUtils.showExceptionDialogNoReport(dotExportPanel, "Couldn't export dot file!", ex);
+                ASUtils.showExceptionDialogNoReport(panel, "Couldn't export dot file!", ex);
             }
         }
     };
     
-    /**
-     * This panel holds all the interface components for graph settings.
-     */
-    private final JPanel dotExportPanel;
+    
+    private Action viewerAutoLayoutAction = new AbstractAction("Auto layout") {
+        
+        public void actionPerformed(ActionEvent e) {
+            doAutoLayout();
+        }
+        
+    };
+
+    private class MyGraphSelectionListener implements GraphSelectionListener<SourceTableRecord, PotentialMatchRecord> {
+
+        public void nodeDeselected(SourceTableRecord node) {
+            recordViewer.removeAll();
+            recordViewer.revalidate();
+        }
+
+        public void nodeSelected(SourceTableRecord node) {
+            try {
+                recordViewer.removeAll();
+                BreadthFirstSearch<SourceTableRecord, PotentialMatchRecord> bfs =
+                    new BreadthFirstSearch<SourceTableRecord, PotentialMatchRecord>();
+                List<SourceTableRecord> reachableNodes = bfs.performSearch(graphModel, node);
+                for (SourceTableRecord str : reachableNodes) {
+                    recordViewer.add(new SourceTableRecordViewer(str, node).getPanel());
+                }
+            } catch (Exception ex) {
+                ASUtils.showExceptionDialog(panel, "Couldn't show potential matches", ex, new MatchMakerQFAFactory());
+            }
+            recordViewer.revalidate();
+        }
+        
+    }
     
     /**
      * This is the match whose result table we're visualizing.
@@ -79,93 +115,54 @@ public class MatchResultVisualizer {
     private final MatchMakerSwingSession session;
 
     /**
-     * The dialog that contains the SettingsPanel.  The constructor
-     * creates it and sets its parent to the SwingSession's frame. 
+     * The panel that holds all the GUI components.
      */
-    private final JDialog dialog;
+    private final JPanel panel;
 
     /**
-     * Text field that holds the pathname for output location for the generated dot file.
+     * The visual component that actually displays the match results graph.
      */
-    private JTextField dotFileField;
-
     private final GraphViewer<SourceTableRecord, PotentialMatchRecord> viewer;
-    
-    /**
-     * Pops up a file chooser for the dotFileField.
-     */
-    private Action pickDotFileAction = new AbstractAction("...") {
-        public void actionPerformed(ActionEvent e) {
-            JFileChooser fc = new JFileChooser();
-            int choice = fc.showDialog(dotExportPanel, "Choose");
-            if (choice == JOptionPane.OK_OPTION) {
-                dotFileField.setText(fc.getSelectedFile().getAbsolutePath());
-            }
-        }
-    };
-    
-    private Action viewerAutoLayoutAction = new AbstractAction("Auto layout") {
-        
-        public void actionPerformed(ActionEvent e) {
-            doAutoLayout();
-        }
-        
-    };
+
+    private final JPanel recordViewer;
+
+    private final MyGraphSelectionListener selectionListener = new MyGraphSelectionListener();
+
+    private final MatchPool pool;
+
+    private final GraphModel<SourceTableRecord, PotentialMatchRecord> graphModel;
 
     public MatchResultVisualizer(Match match, MatchMakerSwingSession session) throws SQLException, ArchitectException {
         this.match = match;
         this.session = session;
-        dotExportPanel = new JPanel();
-        dotExportPanel.add(dotFileField = new JTextField());
-        dotExportPanel.add(new JButton(pickDotFileAction ));
-        dotExportPanel.add(new JButton(exportDotFileAction));
-        
-        File dotFile = new File(
-                System.getProperty("user.home"), "matchmaker_graph_"+match.getName()+".dot");
-        dotFileField.setText(dotFile.getAbsolutePath());
-        
-        MatchPool pool = new MatchPool(match);
+
+        // The top panel
+        JPanel topPanel = new JPanel(new FlowLayout());
+        topPanel.add(new JButton(exportDotFileAction));
+        topPanel.add(new JButton(viewerAutoLayoutAction));
+
+        pool = new MatchPool(match);
         pool.findAll();
-        GraphModel<SourceTableRecord, PotentialMatchRecord> model = new MatchPoolGraphModel(pool);
-        GraphLayoutCache layoutCache = new DefaultGraphLayoutCache();
-        viewer = new GraphViewer<SourceTableRecord, PotentialMatchRecord>(model, layoutCache);
+        graphModel = new MatchPoolGraphModel(pool);
+        viewer = new GraphViewer<SourceTableRecord, PotentialMatchRecord>(graphModel);
         viewer.setNodeRenderer(new SourceTableNodeRenderer());
         viewer.setEdgeRenderer(new PotentialMatchEdgeRenderer(viewer));
-
+        viewer.addSelectionListener(selectionListener );
+        
         doAutoLayout();
         
-        JPanel viewerSettingsPanel = new JPanel(new FlowLayout());
-        viewerSettingsPanel.add(new JButton(viewerAutoLayoutAction));
+        recordViewer = new JPanel(new GridLayout(1, 0));
         
-        dialog = new JDialog(session.getFrame());
+        // put it all together
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+                new JScrollPane(viewer),
+                new JScrollPane(recordViewer, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
         
-        JComponent cp = (JComponent) dialog.getContentPane();
-        cp.setLayout(new BorderLayout());
-        cp.add(viewerSettingsPanel, BorderLayout.NORTH);
-        cp.add(dotExportPanel, BorderLayout.SOUTH);
-        cp.add(new JScrollPane(viewer), BorderLayout.CENTER);
-        
-        dialog.setTitle("Match Result Visualizer");
-        dialog.pack();
-        dialog.setLocationRelativeTo(session.getFrame());
-        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        panel = new JPanel(new BorderLayout(12, 12));
+        panel.add(topPanel, BorderLayout.NORTH);
+        panel.add(splitPane, BorderLayout.CENTER);
     }
     
-    public JPanel getDotExportPanel() {
-        return dotExportPanel;
-    }
-    
-    public void showDialog() {
-        dialog.setVisible(true);
-        dialog.requestFocus();
-    }
-
-    public void refreshGraph() throws SQLException, IOException, ArchitectException {
-        MatchPoolDotExport exporter = new MatchPoolDotExport(match);
-        exporter.setDotFile(new File(dotFileField.getText()));
-        exporter.exportDotFile();
-    }
-
     /**
      * Performs auto-layout on the visible graph of matches managed
      * by this component.
@@ -232,5 +229,23 @@ public class MatchResultVisualizer {
         }
         
         viewer.repaint();
+    }
+    
+    // ======= EditorPane stuff ========
+
+    public boolean doSave() {
+        // TODO Auto-generated method stub
+        logger.debug("Stub call: MatchResultVisualizer.doSave()");
+        return true;
+    }
+
+    public JComponent getPanel() {
+        return panel;
+    }
+
+    public boolean hasUnsavedChanges() {
+        // TODO Auto-generated method stub
+        logger.debug("Stub call: MatchResultVisualizer.hasUnsavedChanges()");
+        return false;
     }
 }
