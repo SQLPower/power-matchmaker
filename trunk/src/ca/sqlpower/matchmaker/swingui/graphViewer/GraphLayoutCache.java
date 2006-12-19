f6ff233c9aabff52fe9c891d715650e66be3d96e
fuerth <fuerth@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-12-19 17:59:23 +0000

    Added type parameters and a new method for finding the node at a certain point

--- trunk/src/ca/sqlpower/matchmaker/swingui/graphViewer/GraphLayoutCache.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/graphViewer/GraphLayoutCache.java
@@ -1,8 +1,17 @@
 package ca.sqlpower.matchmaker.swingui.graphViewer;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 
-public interface GraphLayoutCache {
+/**
+ * The GraphLayoutCache interface specifies a mechanism for maintaining the positions
+ * of nodes and edges in a graph.  It remembers node bounds provided to it, and can
+ * provide hit detection on the items it stores.
+ *
+ * @param <V> The node type for the graph
+ * @param <E> The edge type for the graph
+ */
+public interface GraphLayoutCache<V, E> {
 
     /**
      * Returns the smallest rectangle that completely encloses the
@@ -18,10 +27,21 @@ public interface GraphLayoutCache {
      * @return The last known position and size of the given node.  Returns
      * null if this node is not in the graph.
      */
-    Rectangle getNodeBounds(Object node);
+    Rectangle getNodeBounds(V node);
 
     /**
      * Updates the position and size of the given node in this layout cache.
      */
-    void setNodeBounds(Object node, Rectangle nodePos);
+    void setNodeBounds(V node, Rectangle nodePos);
+    
+    /**
+     * Returns the node that is located under the given point.
+     * 
+     * @param p
+     *            The point to look for a node at
+     * @return The node object at point p, or null if there is not a node on
+     *         that point. If two nodes overlap, returns one or the other
+     *         arbitrarily.
+     */
+    V getNodeAt(Point p);
 }
