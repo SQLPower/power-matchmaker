6dec0b5850cf3a612e5f7a367e14308743cf24a5
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2006-12-20 20:24:12 +0000

    -Made selected graph nodes render differently (just like a JTable cell would) -Added the concept of "focused" graph nodes, and implemented JTable-like rendering for that too -Made a node get focus when you click on its SourceTableRecordViewer

--- src/ca/sqlpower/matchmaker/swingui/SourceTableNodeRenderer.java
+++ src/ca/sqlpower/matchmaker/swingui/SourceTableNodeRenderer.java
@@ -4,20 +4,23 @@ import java.awt.Color;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 
 import ca.sqlpower.matchmaker.SourceTableRecord;
 import ca.sqlpower.matchmaker.swingui.graphViewer.GraphNodeRenderer;
 
-public class SourceTableNodeRenderer extends JLabel implements GraphNodeRenderer<SourceTableRecord> {
+public class SourceTableNodeRenderer extends DefaultTableCellRenderer implements GraphNodeRenderer<SourceTableRecord> {
 
+    private static final JTable DUMMY_TABLE = new JTable(1, 1);
+    
     public SourceTableNodeRenderer() {
         setOpaque(true);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
     
-    public JComponent getGraphNodeRendererComponent(SourceTableRecord node) {
-        setText(node.getKeyValues().toString());
+    public JComponent getGraphNodeRendererComponent(SourceTableRecord node, boolean isSelected, boolean hasFocus) {
+        getTableCellRendererComponent(DUMMY_TABLE, node.getKeyValues().toString(), isSelected, hasFocus, 0, 0);
         return this;
     }
 
