96f71dfa852231c8685cd294002035912ee34498
johnson <johnson@10adfead-6038-0410-9fcd-4bb9978d2467>
2006-12-20 23:05:27 +0000

    extract from one of the editor

---
+++ trunk/src/ca/sqlpower/matchmaker/swingui/CheckBoxRenderer.java
@@ -0,0 +1,33 @@
+package ca.sqlpower.matchmaker.swingui;
+
+import java.awt.Component;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ * A renderer for presenting a JCheckBox in a JTable cell.  It makes sure the check box
+ * looks right (correct foreground and background colour depending on the cell's selection
+ * state) in the table.
+ */
+public class CheckBoxRenderer extends JCheckBox implements TableCellRenderer {
+
+    CheckBoxRenderer() {
+        setHorizontalAlignment(JLabel.CENTER);
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if (isSelected) {
+            setForeground(table.getSelectionForeground());
+            setBackground(table.getSelectionBackground());
+        } else {
+            setForeground(table.getForeground());
+            setBackground(table.getBackground());
+        }
+System.out.println("*** value==null? "+(value==null) + "  "+(value==null?false:((Boolean) value).booleanValue()));        
+        setSelected((value != null && ((Boolean) value).booleanValue()));
+        return this;
+    }
+}
