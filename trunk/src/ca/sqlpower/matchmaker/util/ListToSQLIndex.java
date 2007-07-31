67361f5f169bf08e2fc29f22afd8122c8fd9ecff
tmckee <tmckee@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-07-31 16:01:47 +0000

    Added try/catch blocks to catch ArchitectExceptions. Since the methods implement an interface that only throw HibernateExceptions, I have them throw ArchitectRuntimeExceptions so that they can get caught by the uncaught exception handler later.

--- trunk/src/ca/sqlpower/matchmaker/util/ListToSQLIndex.java
+++ trunk/src/ca/sqlpower/matchmaker/util/ListToSQLIndex.java
@@ -11,6 +11,8 @@ import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.usertype.UserType;
 
+import ca.sqlpower.architect.ArchitectException;
+import ca.sqlpower.architect.ArchitectRuntimeException;
 import ca.sqlpower.architect.SQLIndex;
 
 /**
@@ -38,23 +40,27 @@ public class ListToSQLIndex implements UserType  {
                 oldIndex.getQualifier(),
                 oldIndex.getType(),
                 oldIndex.getFilterCondition());
-        for (Object child : oldIndex.getChildren()) {
-        	SQLIndex.Column oldIndexColumn = (SQLIndex.Column) child;
+        try {
+			for (Object child : oldIndex.getChildren()) {
+				SQLIndex.Column oldIndexColumn = (SQLIndex.Column) child;
 
-        	SQLIndex.Column c;
-        	if (oldIndexColumn.getColumn() != null) {
-        		c = newIndex.new Column(
-        				oldIndexColumn.getColumn(),
-        				oldIndexColumn.isAscending(),
-        				oldIndexColumn.isDescending());
-        	} else {
-        		c = newIndex.new Column(
-        				oldIndexColumn.getName(),
-        				oldIndexColumn.isAscending(),
-        				oldIndexColumn.isDescending());
-        	}
-        	newIndex.addChild(c);
-        }
+				SQLIndex.Column c;
+				if (oldIndexColumn.getColumn() != null) {
+					c = newIndex.new Column(
+							oldIndexColumn.getColumn(),
+							oldIndexColumn.isAscending(),
+							oldIndexColumn.isDescending());
+				} else {
+					c = newIndex.new Column(
+							oldIndexColumn.getName(),
+							oldIndexColumn.isAscending(),
+							oldIndexColumn.isDescending());
+				}
+				newIndex.addChild(c);
+			}
+		} catch (ArchitectException e) {
+			throw new ArchitectRuntimeException(e);
+		}
         return newIndex;
     }
 
@@ -76,27 +82,31 @@ public class ListToSQLIndex implements UserType  {
         if ((x instanceof SQLIndex) && (y instanceof SQLIndex)) {
         	SQLIndex indexX = (SQLIndex) x;
         	SQLIndex indexY = (SQLIndex) y;
-        	if (indexX.getName() == null ? indexX.getName() != indexY.getName():!indexX.getName().equals(indexY.getName())){
-        		if (logger.isDebugEnabled()){
-        			logger.debug("different pk name was " + indexY.getName()+ " expecting "+ indexX.getName());
-        		}
-        		return false;
-        	} else if (indexX.getChildCount() != indexY.getChildCount()) {
-        		if (logger.isDebugEnabled()){
-        			logger.debug("different child count was "+ indexY.getChildCount()+ " expecting "+ indexX.getChildCount());
-        		}
-        		return false;
-        	} else {
-        		for (int i=0; i < indexX.getChildCount(); i++){
-        			if (indexX.getChild(i).getName() == null ? indexX.getChild(i).getName() != indexY.getChild(i).getName():!indexX.getChild(i).getName().equals(indexY.getChild(i).getName())){
-        				if (logger.isDebugEnabled()){
-        					logger.debug("different column name was " + indexY.getChild(i).getName()+ " expecting "+ indexX.getChild(i).getName());
-        				}
-        				return false;
-        			}
-        		}
-        		return true;
-        	}
+        	try {
+				if (indexX.getName() == null ? indexX.getName() != indexY.getName():!indexX.getName().equals(indexY.getName())){
+					if (logger.isDebugEnabled()){
+						logger.debug("different pk name was " + indexY.getName()+ " expecting "+ indexX.getName());
+					}
+					return false;
+				} else if (indexX.getChildCount() != indexY.getChildCount()) {
+					if (logger.isDebugEnabled()){
+						logger.debug("different child count was "+ indexY.getChildCount()+ " expecting "+ indexX.getChildCount());
+					}
+					return false;
+				} else {
+					for (int i=0; i < indexX.getChildCount(); i++){
+						if (indexX.getChild(i).getName() == null ? indexX.getChild(i).getName() != indexY.getChild(i).getName():!indexX.getChild(i).getName().equals(indexY.getChild(i).getName())){
+							if (logger.isDebugEnabled()){
+								logger.debug("different column name was " + indexY.getChild(i).getName()+ " expecting "+ indexX.getChild(i).getName());
+							}
+							return false;
+						}
+					}
+					return true;
+				}
+			} catch (ArchitectException e) {
+				throw new ArchitectRuntimeException(e);
+			}
         } else {
             return false;
         }
@@ -124,9 +134,13 @@ public class ListToSQLIndex implements UserType  {
                 if (names[i] != null) {
                     final String columnName = rs.getString(rs.findColumn(names[i]));
                     if (columnName != null) {
-                    	SQLIndex.Column c;
-                    	c = index.new Column(columnName, false, false);
-                    	index.addChild(c);
+                    	try {
+							SQLIndex.Column c;
+							c = index.new Column(columnName, false, false);
+							index.addChild(c);
+						} catch (ArchitectException e) {
+							throw new ArchitectRuntimeException(e);
+						}
                     }
                 }
             }
@@ -148,23 +162,27 @@ public class ListToSQLIndex implements UserType  {
                 logger.debug("           setting param " + indexItemPos + " to \"" + ind.getName() + "\"");
             }
             st.setString(indexItemPos, ind.getName());
-            // It is require to increment the index by 1 since the inital
-            // index is for the SQLIndex name. The index needs to increase
-            // to synchronize with setting the values of the columns
-            for (SQLIndex.Column c : (List<SQLIndex.Column>) ind.getChildren()) {
-            	indexItemPos++;
-            	if (logger.isDebugEnabled()) {
-            		logger.debug("           setting param " + indexItemPos + " to \"" + c.getName() + "\"");
-            	}
-            	st.setString(indexItemPos, c.getName());
-            }
-            // fill in the rest of the values
-            for (int i = indexItemPos + 1; i < COLUMN_COUNT + index; i++) {
-            	if (logger.isDebugEnabled()) {
-            		logger.debug("           setting param " + i + " to \"" + null + "\"");
-            	}
-            	st.setNull(i, Types.VARCHAR);
-            }
+            try {
+				// It is required to increment the index by 1 since the inital
+				// index is for the SQLIndex name. The index needs to increase
+				// to synchronize with setting the values of the columns
+				for (SQLIndex.Column c : (List<SQLIndex.Column>) ind.getChildren()) {
+					indexItemPos++;
+					if (logger.isDebugEnabled()) {
+						logger.debug("           setting param " + indexItemPos + " to \"" + c.getName() + "\"");
+					}
+					st.setString(indexItemPos, c.getName());
+				}
+				// fill in the rest of the values
+				for (int i = indexItemPos + 1; i < COLUMN_COUNT + index; i++) {
+					if (logger.isDebugEnabled()) {
+						logger.debug("           setting param " + i + " to \"" + null + "\"");
+					}
+					st.setNull(i, Types.VARCHAR);
+				}
+			} catch (ArchitectException e) {
+				throw new ArchitectRuntimeException(e);
+			}
         } else if (value == null) {
         	for (int i = index; i < COLUMN_COUNT + index; i++) {
                 if (logger.isDebugEnabled()) {
