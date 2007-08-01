0588bb731a41e3e85d95b9cd9a7d5886036029cc
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2007-08-01 17:30:51 +0000

    Refactored database connection manager. Fixed compile errors, and mostly reused the new system we made for Architect yesterday.

--- src/ca/sqlpower/matchmaker/swingui/DBConnectionUniDialog.java
+++
@@ -1,9 +0,0 @@
-package ca.sqlpower.matchmaker.swingui;
-
-import javax.swing.JDialog;
-
-public interface DBConnectionUniDialog {
-	JDialog getNewConnectionDialog();
-
-	void setNewConnectionDialog(JDialog d);
-}
