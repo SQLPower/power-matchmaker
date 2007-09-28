cc5e002991fe767cafa5cfc70f5e56865b537a69
jfuerth <jfuerth@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-09-28 20:46:42 +0000

    New generic change watcher class that should help editor panes implement the hasUnsavedChanges feature more easily and correctly.

---
+++ trunk/src/ca/sqlpower/matchmaker/swingui/MMOChangeWatcher.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2007, SQL Power Group Inc.
+ *
+ * This file is part of Power*MatchMaker.
+ *
+ * Power*MatchMaker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Power*MatchMaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.matchmaker.swingui;
+
+import ca.sqlpower.matchmaker.MatchMakerObject;
+import ca.sqlpower.matchmaker.MatchMakerUtils;
+import ca.sqlpower.matchmaker.event.MatchMakerEvent;
+import ca.sqlpower.matchmaker.event.MatchMakerListener;
+
+/**
+ * Utility class that registers every change to a subtree of MatchMakerObjects
+ * by flipping its hasChanged flag to true. Mainly intended to support the
+ * hasUnsavedChanges feature of EditorPane.
+ */
+public class MMOChangeWatcher <T extends MatchMakerObject,C extends MatchMakerObject>
+implements MatchMakerListener<T,C> {
+
+    private boolean hasChanged = false;
+
+    /**
+     * Creates a new MMO Change Watcher for the matchmaker object subtree rooted
+     * at the given node.
+     * 
+     * @param mmo The root node of the subtree to monitor.
+     */
+    public MMOChangeWatcher(MatchMakerObject<T,C> mmo) {
+        MatchMakerUtils.listenToHierarchy(this, mmo);
+    }
+
+    /**
+     * Listener implementation.
+     */
+    public void mmChildrenInserted(MatchMakerEvent<T,C> evt) {
+        hasChanged = true;
+        for (MatchMakerObject<T,C> child : evt.getChildren()) {
+            MatchMakerUtils.listenToHierarchy(this, child);
+        }
+    }
+
+    /**
+     * Listener implementation.
+     */
+    public void mmChildrenRemoved(MatchMakerEvent<T,C> evt) {
+        hasChanged = true;
+        for (MatchMakerObject<T,C> child : evt.getChildren()) {
+            MatchMakerUtils.unlistenToHierarchy(this, child);
+        }
+    }
+
+    /**
+     * Listener implementation.
+     */
+    public void mmPropertyChanged(MatchMakerEvent<T,C> evt) {
+        hasChanged = true;
+    }
+
+    /**
+     * Listener implementation.
+     */
+    @SuppressWarnings("unchecked")
+    public void mmStructureChanged(MatchMakerEvent<T,C> evt) {
+        hasChanged = true;
+        MatchMakerUtils.listenToHierarchy(this, evt.getSource());
+    }
+
+    /**
+     * Returns the last value given to {@link #setHasChanged(boolean)}, unless this object
+     * has received any MatchMakerObject events since then, in which case the return value
+     * will be <tt>true</tt>.
+     * 
+     * @return True if there have been any MatchMakerObject events since the last call to
+     * setHasChanged; otherwise, the most recent value set in setHasChanged.
+     */
+    public boolean getHasChanged() {
+        return hasChanged;
+    }
+
+    /**
+     * Sets the hasChanged flag to the given value. Normally, you would call
+     * this method to set the status back to false just after you save the
+     * subtree being monitored by this watcher.
+     * 
+     * @param hasChanged The new hasChanged status.
+     */
+    public void setHasChanged(boolean hasChanged) {
+        this.hasChanged = hasChanged;
+    }
+
+}
