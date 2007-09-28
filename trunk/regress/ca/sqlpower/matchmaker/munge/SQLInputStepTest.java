cc01b94f700f30fa09737eb3ceaa509520c821de
jfuerth <jfuerth@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-09-28 19:18:18 +0000

    New step implementation that gets its data from a SQL query, and advances the query result set by one row on each call().
    Currently, you have to give this step a SQLTable directly when you create it, but in the long run, it will have to look for the input table in its ancestor Match object.  I haven't implemented that yet because before we can do that, we have to get rid of the existing MatchRule class and substitute it for MungeStep in the MMO hierarchy.

---
+++ trunk/regress/ca/sqlpower/matchmaker/munge/SQLInputStepTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2007, SQL Power Group Inc.
+ *
+ * This file is part of Power*MatchMaker.
+ *
+ * Power*MatchMaker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Power*MatchMaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.matchmaker.munge;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Types;
+import java.util.Date;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+import ca.sqlpower.architect.SQLColumn;
+import ca.sqlpower.architect.SQLDatabase;
+import ca.sqlpower.architect.SQLTable;
+import ca.sqlpower.matchmaker.Match;
+import ca.sqlpower.testutil.MockJDBCConnection;
+import ca.sqlpower.testutil.MockJDBCResultSet;
+
+public class SQLInputStepTest extends TestCase {
+
+    private class FakeDatabase extends SQLDatabase {
+        
+        private MockJDBCConnection con;
+
+        public FakeDatabase() {
+            String url = "jdbc:mock:tables=table1";
+            con = new MockJDBCConnection(url, new Properties());
+            
+            MockJDBCResultSet rs = new MockJDBCResultSet(3);
+            rs.addRow(new Object[] {"row1,1", new Integer(12), new Date(1234)});
+            rs.addRow(new Object[] {null,     null,            null});
+            rs.addRow(new Object[] {"row3,1", new Integer(32), new Date(5678)});
+            
+            con.registerResultSet("SELECT.*FROM table1.*", rs);
+        }
+        
+        public Connection getConnection() {
+            return con;
+        }
+    }
+    
+    SQLInputStep step;
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        SQLDatabase db = new FakeDatabase();
+        SQLTable table = new SQLTable(db, true);
+        db.addChild(table);
+        
+        table.setName("table1");
+        table.addColumn(new SQLColumn(table, "col1", Types.VARCHAR, 50, 0));
+        table.addColumn(new SQLColumn(table, "col2", Types.INTEGER, 50, 0));
+        table.addColumn(new SQLColumn(table, "col3", Types.TIMESTAMP, 50, 0));
+
+        Match match = new Match();
+        match.setSourceTable(table);
+        step = new SQLInputStep(table);
+    }
+    
+    public void testNoDoubleOpen() throws Exception {
+        step.open();
+        try {
+            step.open();
+            fail("Opened step twice (should not be possible)");
+        } catch (IllegalStateException ex) {
+            // good
+        }
+        step.close();
+    }
+    
+    public void testCallReturnsCorrectValues() throws Exception {
+        step.open();
+        assertTrue(step.call());
+        assertTrue(step.call());
+        assertTrue(step.call());
+        assertFalse(step.call());
+        step.close();
+    }
+    
+    public void testOutputsNullAtFirst() throws Exception {
+        step.open();
+        assertNull(step.getChildren().get(0).getData());
+        assertNull(step.getChildren().get(1).getData());
+        assertNull(step.getChildren().get(2).getData());
+        step.close();
+    }
+
+    public void testOutputsNullAfterEnd() throws Exception {
+        step.open();
+        while (step.call() == Boolean.TRUE);
+        assertNull(step.getChildren().get(0).getData());
+        assertNull(step.getChildren().get(1).getData());
+        assertNull(step.getChildren().get(2).getData());
+        step.close();
+    }
+
+    public void testOutputsTrackResults() throws Exception {
+        step.open();
+        step.call();
+        assertEquals("row1,1",             step.getChildren().get(0).getData());
+        assertEquals(new BigDecimal("12"), step.getChildren().get(1).getData());
+        assertEquals(new Date(1234),       step.getChildren().get(2).getData());
+
+        step.call();
+        assertEquals(null, step.getChildren().get(0).getData());
+        assertEquals(null, step.getChildren().get(1).getData());
+        assertEquals(null, step.getChildren().get(2).getData());
+
+        step.call();
+        assertEquals("row3,1",             step.getChildren().get(0).getData());
+        assertEquals(new BigDecimal("32"), step.getChildren().get(1).getData());
+        assertEquals(new Date(5678),       step.getChildren().get(2).getData());
+        
+        step.close();
+    }
+}
