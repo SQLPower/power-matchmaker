d4aba8de2f17ec942a929f5c5d50b9724e2038bf
sean.oczkowski <sean.oczkowski@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-10-03 21:22:10 +0000

    Changed the IOConnection around so that it is now a JComponent and maintains its own bounds and handles its own events.
    It now continuously sets it bounds and draws itself.
    
    Removed the list of lines from the munge pen as this is now handled by the component superclass.
    
    Each line is now a mouse listener for the munge pen so that they can properly handle the click events of the lines, before if a line was clicked within the bounds the top most line would steal the event even if there was one under it.
    
    Changed the munge component so that the window will immediately redraw the scroll pane after a component is moved outside the frame.
    
    Re-added the right click menu to the lines.
    
    Munge Pen is now in a good condition.

--- trunk/src/ca/sqlpower/matchmaker/swingui/munge/IOConnector.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/munge/IOConnector.java
@@ -27,12 +27,23 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Line2D;
 
 import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+
+import org.apache.log4j.Logger;
 
 import ca.sqlpower.matchmaker.munge.MungeStepOutput;
 
@@ -41,13 +52,20 @@ import ca.sqlpower.matchmaker.munge.MungeStepOutput;
  *This does not draw the line but holds its position.
  *Each IOC is recreated every time the munge pen is redrawn 
  */
-public class IOConnector {
-	AbstractMungeComponent parent;
-	AbstractMungeComponent child;
+public class IOConnector extends JComponent implements MouseListener {
+	
+	private static  final Logger logger = Logger.getLogger(IOConnector.class); 
+	
+	AbstractMungeComponent parentCom;
+	AbstractMungeComponent childCom;
 	int parentNumber;
 	int childNumber;
 	
-	boolean selected;
+	
+	private Point top;
+	private Point bottom;
+	
+	private boolean recentlyResized;
 	
 	/**
 	 * Creates a new IOConnector that represents a connection line in the mungePen
@@ -58,11 +76,48 @@ public class IOConnector {
 	 * @param childNum The number of the IOC in the child
 	 */
 	public IOConnector(AbstractMungeComponent parent, int parentNum, AbstractMungeComponent child, int childNum) {
-		this.parent = parent;
-		this.child = child;
+		this.parentCom = parent;
+		this.childCom = child;
 		parentNumber = parentNum;
 		childNumber = childNum;
-		selected = false;
+		
+		setOpaque(false);
+		
+		setFocusable(true);
+		
+		addFocusListener(new FocusListener(){
+			public void focusGained(FocusEvent e) {
+				//a problem it the line was removed and this IOC has not been told that yet.
+				if (getPen() != null) {
+					logger.debug("Gained focus");
+					requestFocusInWindow();
+					getParent().repaint();
+				}
+			}
+			
+			public void focusLost(FocusEvent e) {
+				if (getPen() != null) {
+					logger.debug("lost focus");
+					getParent().repaint();
+				}
+			}
+		});
+		
+		addKeyListener(new KeyAdapter(){
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_DELETE) {
+					remove();
+				}
+			}
+		});
+		
+		recentlyResized = false;
+		setBounds();
+	}
+
+	public MungePen getPen() {
+		return (MungePen)getParent();
 	}
 	
 	
@@ -74,11 +129,11 @@ public class IOConnector {
 	 * click
 	 */
 	public boolean clicked(Point click) {
-		Point p1 = parent.getOutputPosition(parentNumber);
-		p1.translate(parent.getX(), parent.getY());
-		Point p2 = child.getInputPosition(childNumber);
-		p2.translate(child.getX(), child.getY());
-		double dist = Line2D.ptLineDist(p1.x,p1.y,p2.x,p2.y,click.x,click.y);
+		Point p1 = parentCom.getOutputPosition(parentNumber);
+		p1.translate(parentCom.getX(), parentCom.getY());
+		Point p2 = childCom.getInputPosition(childNumber);
+		p2.translate(childCom.getX(), childCom.getY());
+		double dist = Line2D.ptSegDist(p1.x,p1.y,p2.x,p2.y,click.x,click.y);
 		return dist < 6;
 	}
 
@@ -105,7 +160,7 @@ public class IOConnector {
 	public boolean equals(Object obj) {
 		if (obj instanceof IOConnector) {
 			IOConnector ioc = (IOConnector) obj;
-			return ioc.parent == parent && ioc.child == child && ioc.parentNumber == parentNumber && ioc.childNumber == childNumber;
+			return ioc.parentCom == parentCom && ioc.childCom == childCom && ioc.parentNumber == parentNumber && ioc.childNumber == childNumber;
 		}
 		return false;
 	}
@@ -121,56 +176,132 @@ public class IOConnector {
 			remove();
 		}
 	}
+	
+	public void setBounds() {
 
+		if (recentlyResized) {
+			return;
+		}
+		
+		recentlyResized = true;
+		
+		bottom = childCom.getInputPosition(childNumber);
+		top = parentCom.getOutputPosition(parentNumber);
+		top.translate(parentCom.getX(), parentCom.getY());
+		bottom.translate(childCom.getX(), childCom.getY());
+		
+		int width;
+		int height;
+		int x;
+		int y;
+		
+		width = Math.abs(bottom.x - top.x);
+		height = Math.abs(bottom.y - top.y);
+		
+		
+		if (top.getY() < bottom.getY()) {
+			if (top.getX() < bottom.getX()) {
+				x = top.x;
+				y = top.y;
+				bottom = new Point(width,height);
+				top = new Point(0,0);
+			} else {
+				x = bottom.x;
+				y = top.y;
+				bottom = new Point(0, height);
+				top = new Point(width, 0);
+			} 
+		} else {
+			if (top.getX() < bottom.getX()) {
+				x = top.x;
+				y = bottom.y;
+				bottom = new Point(width, 0);
+				top = new Point(0, height);
+			} else {
+				x = bottom.x;
+				y = bottom.y;
+				bottom = new Point(width,height);
+				top = new Point(0,0);
+			} 
+		}
+		
+		setBounds(x,y,width+1,height+1);
+	}
+	
+
+	
+	@Override
+	public int getWidth() {
+		setBounds();
+		return super.getWidth();
+	}
+	
+	@Override
+	public int getHeight() {
+		setBounds();
+		return super.getHeight();
+	}
+	
+	@Override
+	public int getX() {
+		setBounds();
+		return super.getX();
+	}
+	
+	@Override
+	public int getY() {
+		setBounds();
+		return super.getY();
+	}
+
+
+	
 	/**
 	 * paints this IOC as a line on the specified Graphics
 	 * 
 	 * @param g The place to paint
 	 */
-	public void paint(Graphics g) {
-		if (selected) {
+	public void paintComponent(Graphics g) {
+		recentlyResized = false;
+		
+		if (hasFocus()) {
 			((Graphics2D)g).setStroke(new BasicStroke(2));
 		}
 		
-		Point bottom = child.getInputPosition(childNumber);
-		Point top = parent.getOutputPosition(parentNumber);
 		
-		top.translate(parent.getX(), parent.getY());
-		bottom.translate(child.getX(), child.getY());
+		MungeStepOutput link = childCom.getInputs().get(childNumber);
 		
-		MungeStepOutput link = child.getInputs().get(childNumber);
-		g.setColor(AbstractMungeComponent.getColor(link.getType()));
+		//If it is drawing in the time period between the child removed 
+		//and the event being finished,
+		if (link != null) {
+			g.setColor(AbstractMungeComponent.getColor(link.getType()));			
+			g.drawLine((int)top.getX(), (int)top.getY(), (int)bottom.getX(), (int)bottom.getY());
+		} else {
+			logger.debug("Error line not existant");
+		}
 		
-		g.drawLine((int)top.getX(), (int)top.getY(), (int)bottom.getX(), (int)bottom.getY());
-		
-		if (selected) {
+		if (hasFocus()) {
 			((Graphics2D)g).setStroke(new BasicStroke(1));
 		}
-	}
-	
-	/**
-	 * Sets this IOC to be selected so that it can be drawn diffrently
-	 * 
-	 * @param sel Set to true to select this component, false otherwise 
-	 */
-	public void setSelected(boolean sel) {
-		selected = sel;
+		
 	}
 	
 	/**
 	 * deletes this IOC and the link in the mungeSteps 
 	 */
 	public void remove() {
-		child.getStep().disconnectInput(childNumber);
+		getParent().removeMouseListener(this);
+		removeAllListeners();
+		childCom.getStep().disconnectInput(childNumber);
 	}
 
 
-	public AbstractMungeComponent getParent() {
-		return parent;
+	public AbstractMungeComponent getParentCom() {
+		return parentCom;
 	}
 
-	public AbstractMungeComponent getChild() {
-		return child;
+	public AbstractMungeComponent getChildCom() {
+		return childCom;
 	}
 
 
@@ -180,5 +311,69 @@ public class IOConnector {
 
 	public int getChildNumber() {
 		return childNumber;
+	}
+
+	//mouseListener Stuff, each IOC is a mouse listener of
+	//the MungePen to allow it to properly handle the overlap
+	
+	public void mouseClicked(MouseEvent e) {
+		if (clicked(e.getPoint())) {
+			maybeShowPopup(e);
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		logger.debug(e.getPoint());
+		if (clicked(e.getPoint())) {
+			requestFocusInWindow();
+			maybeShowPopup(e);
+		}
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		if (clicked(e.getPoint())) {
+			maybeShowPopup(e);
+		}
+	}
+	
+	public void maybeShowPopup(MouseEvent e) {
+		if (e.isPopupTrigger()) {
+			logger.debug("ShowPopop");
+			getPopup().show(getParent(), e.getX(), e.getY());
+			requestFocusInWindow();
+		}
+	}
+
+	/**
+	 * Removes all listeners associated with this IOC. This is useful when removing to to make sure
+	 * it does not stick around.
+	 */
+	public void removeAllListeners() {
+		for (FocusListener fl : getFocusListeners()) {
+			removeFocusListener(fl);
+		}
+		
+		for (MouseListener ml : getMouseListeners()) {
+			removeMouseListener(ml);
+		}
+		
+		for (MouseMotionListener mml : getMouseMotionListeners()) {
+			removeMouseMotionListener(mml);
+		}
+		
+		for (KeyListener kl : getKeyListeners()) {
+			removeKeyListener(kl);
+		}
+		
+		for (ComponentListener cl : getComponentListeners()) {
+			removeComponentListener(cl);
+		}	
 	}
 }
