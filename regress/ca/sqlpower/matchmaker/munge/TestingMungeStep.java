11c92bb30d90aa896d2f19de9beba01e217e67cc
jeffrey <jeffrey@sqlpower.ca>
2007-10-03 21:39:39 +0000

    * Renamed MungeProcessorTest to MungeProcessorImplTest, since we're really testing the MungeProcessorImpl. Also implemented the 'correct step order' test case * Fleshed out the default implementation of the MungeProcessor * Modified TestingMungeStep to have a isContinuing parameter, so that in tests, we can actually stop the munge process

--- regress/ca/sqlpower/matchmaker/munge/TestingMungeStep.java
+++ regress/ca/sqlpower/matchmaker/munge/TestingMungeStep.java
@@ -19,6 +19,8 @@
 
 package ca.sqlpower.matchmaker.munge;
 
+import org.apache.log4j.Logger;
+
 /**
  * A simple munge step implementation that can be created with any number of inputs
  * and outputs.  The outputs are always null, and the <tt>call</tt> method does
@@ -26,6 +28,31 @@ package ca.sqlpower.matchmaker.munge;
  */
 public class TestingMungeStep extends AbstractMungeStep {
 
+	private static final Logger logger = Logger.getLogger(TestingMungeStep.class);
+	
+	/**
+	 * Indicates whether this test step will return false 
+	 * from the {@link #call()} method.
+	 */
+	private boolean continuing;
+	
+    /**
+     * Creates a new instance with the given name, number of input slots, 
+     * and number of outputs (which are children of the step from the tree
+     * model point of view).  All inputs and outputs are of type String.
+     * <p>
+     * By default, this testing munge step will return false from its {@link #call()}
+     * method. To make it return false, use {@ #TestingMungeStep(String, int, int, boolean)}.
+     * 
+     * @param name This step's name
+     * @param inputs The number of input slots for this step. All slots will
+     * be initially not connected.
+     * @param outputs The number of outputs for this step
+     */
+    public TestingMungeStep(String name, int inputs, int outputs) {
+        this(name, inputs, outputs, true);
+    }
+        
     /**
      * Creates a new instance with the given name, number of input slots, 
      * and number of outputs (which are children of the step from the tree
@@ -35,8 +62,10 @@ public class TestingMungeStep extends AbstractMungeStep {
      * @param inputs The number of input slots for this step. All slots will
      * be initially not connected.
      * @param outputs The number of outputs for this step
+     * @param continuing Set to true if the the MungeProcess should continue
+     * after this step. Otherwise, return false.
      */
-    public TestingMungeStep(String name, int inputs, int outputs) {
+    public TestingMungeStep(String name, int inputs, int outputs, boolean continuing) {
         setName(name);
         for (int i = 0; i < inputs; i++) {
             addInput(new InputDescriptor("input_" + i, String.class));
@@ -44,6 +73,17 @@ public class TestingMungeStep extends AbstractMungeStep {
         for (int i = 0; i < outputs; i++) {
             addChild(new MungeStepOutput<String>("output_"+i, String.class));
         }
+        this.continuing = continuing;
+    }
+    
+    /**
+     * Returns true if the parameter {@link #continuing} is set to true. Otherwise,
+     * returns false.
+     */
+    public Boolean call() throws Exception {
+    	super.call();
+    	logger.debug("Step '" + getName() + "' is being called");
+    	return continuing;
     }
     
     /**
@@ -53,4 +93,8 @@ public class TestingMungeStep extends AbstractMungeStep {
         return false;
     }
     
+    @Override
+    public String toString() {
+    	return getName();
+    }
 }
