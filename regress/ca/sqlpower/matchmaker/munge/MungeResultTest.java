9354c1c15e3373f59b3b6add07f9bea01f7328c3
jeffrey <jeffrey@sqlpower.ca>
2007-10-15 22:50:10 +0000

    Fixed test to reflect that MungeResults contain just the munged data, instead of containing the MungeStepOutputs

--- regress/ca/sqlpower/matchmaker/munge/MungeResultTest.java
+++ regress/ca/sqlpower/matchmaker/munge/MungeResultTest.java
@@ -34,16 +34,8 @@ public class MungeResultTest extends TestCase {
 
 	private MungeResult r1;
 	private MungeResult r2;
-	private MungeStepOutput<String> outString1;
-	private MungeStepOutput<String> outString2;
-	private MungeStepOutput<BigDecimal> outDecimal1;
-	private MungeStepOutput<BigDecimal> outDecimal2;
-	private MungeStepOutput<Boolean> outBoolean1;
-	private MungeStepOutput<Boolean> outBoolean2;
-	private MungeStepOutput<Date> outDate1;
-	private MungeStepOutput<Date> outDate2;
-	private MungeStepOutput[] mungedData2;
-	private MungeStepOutput[] mungedData1;
+	private Object[] mungedData1;
+	private Object[] mungedData2;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -59,26 +51,10 @@ public class MungeResultTest extends TestCase {
 		keyValues.add("test2");
 		r2.setSourceTableRecord(new SourceTableRecord(session, new Match(), keyValues));
 		
-		outString1 = new MungeStepOutput<String>("string", String.class);
-		outString1.setData("B");
-		outDecimal1 = new MungeStepOutput<BigDecimal>("bigdecimal", BigDecimal.class);
-		outDecimal1.setData(BigDecimal.valueOf(1));
-		outBoolean1 = new MungeStepOutput<Boolean>("boolean", Boolean.class);
-		outBoolean1.setData(Boolean.TRUE);
-		outDate1 = new MungeStepOutput<Date>("date", Date.class);
-		outDate1.setData(new Date(1));
-		mungedData1 = new MungeStepOutput[] {outString1, outDecimal1, outBoolean1, outDate1};
+		mungedData1 = new Object[] {"B", BigDecimal.valueOf(1), Boolean.TRUE, new Date(1)};
 		r1.setMungedData(mungedData1);
 		
-		outString2 = new MungeStepOutput<String>("string", String.class);
-		outString2.setData("B");
-		outDecimal2 = new MungeStepOutput<BigDecimal>("bigdecimal", BigDecimal.class);
-		outDecimal2.setData(BigDecimal.valueOf(1));
-		outBoolean2 = new MungeStepOutput<Boolean>("boolean", Boolean.class);
-		outBoolean2.setData(Boolean.TRUE);
-		outDate2 = new MungeStepOutput<Date>("date", Date.class);
-		outDate2.setData(new Date(1));
-		mungedData2 = new MungeStepOutput[] {outString2, outDecimal2, outBoolean2, outDate2};
+		mungedData2 = new Object[] {"B", BigDecimal.valueOf(1), Boolean.TRUE, new Date(1)};
 		r2.setMungedData(mungedData2);
 	}
 
@@ -88,27 +64,27 @@ public class MungeResultTest extends TestCase {
 	}
 	
 	public void testCompareToString() throws Exception {
-		outString2.setData("A");
+		mungedData2[0] = "A";
 		int compareResult = r1.compareTo(r2);
 		assertEquals(1, compareResult);
 		
-		outString2.setData("C");
+		mungedData2[0] = "C";
 		compareResult = r1.compareTo(r2);
 		assertEquals(-1, compareResult);
 	}
 	
 	public void testCompareToDecimal() throws Exception {
-		outDecimal2.setData(BigDecimal.valueOf(0));
+		mungedData2[1] = BigDecimal.valueOf(0);
 		int compareResult = r1.compareTo(r2);
 		assertEquals(1, compareResult);
 		
-		outDecimal2.setData(BigDecimal.valueOf(2));
+		mungedData2[1] = BigDecimal.valueOf(2);
 		compareResult = r1.compareTo(r2);
 		assertEquals(-1, compareResult);
 	}
 	
 	public void testCompareToBoolean() throws Exception {
-		outBoolean2.setData(Boolean.FALSE);
+		mungedData2[2] = Boolean.FALSE;
 		int compareResult = r1.compareTo(r2);
 		assertEquals(1, compareResult);
 		
@@ -117,11 +93,11 @@ public class MungeResultTest extends TestCase {
 	}
 	
 	public void testCompareToDate() throws Exception {
-		outDate2.setData(new Date(0));
+		mungedData2[3] = new Date(0);
 		int compareResult = r1.compareTo(r2);
 		assertEquals(1, compareResult);
 		
-		outDate2.setData(new Date(2));
+		mungedData2[3] = new Date(2);
 		compareResult = r1.compareTo(r2);
 		assertEquals(-1, compareResult);
 	}
@@ -129,13 +105,13 @@ public class MungeResultTest extends TestCase {
 	// Test to ensure the String, which is first in the list of MungeStepOutputs
 	// takes precedence over the BigDecimal output
 	public void testCompareToStringAndDecimal() throws Exception {
-		outString2.setData("A");
-		outDecimal2.setData(BigDecimal.valueOf(2));
+		mungedData2[0] = "A";
+		mungedData2[1] = BigDecimal.valueOf(2);
 		int compareResult = r1.compareTo(r2);
 		assertEquals(1, compareResult);
 		
-		outString2.setData("C");
-		outDecimal2.setData(BigDecimal.valueOf(0));
+		mungedData2[0] = "C";
+		mungedData2[1] = BigDecimal.valueOf(0);
 		compareResult = r1.compareTo(r2);
 		assertEquals(-1, compareResult);
 	}
