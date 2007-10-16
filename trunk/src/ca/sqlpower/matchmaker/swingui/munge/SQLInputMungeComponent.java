51ba92a548bf52e1018fdbfe45304ff7d57f4cfd
sean.oczkowski <sean.oczkowski@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-10-16 20:54:50 +0000

    Moved the code that generates the output labels for the starting step to the munge component.
    Modified it to allow any component to show its input or output names by calling setInputShowNames(true) and setOutputShowNames(true) respectivly.
    
    Moved the hide/show all buttons there as well and they can now be used by any subclass.
    
    Cleaned up the concat step so that it uses a simple flow layout instead of a form layout because it was overcomplicated and caused issues during testing.
    
    Cleaned up to InputStep to simply use the new code in the abstract munge component.

--- trunk/src/ca/sqlpower/matchmaker/swingui/munge/SQLInputMungeComponent.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/munge/SQLInputMungeComponent.java
@@ -19,27 +19,16 @@
 
 package ca.sqlpower.matchmaker.swingui.munge;
 
-import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
-import javax.swing.ToolTipManager;
 
 import org.apache.log4j.Logger;
 
 import ca.sqlpower.matchmaker.MatchMakerSession;
 import ca.sqlpower.matchmaker.munge.MungeStep;
-import ca.sqlpower.matchmaker.munge.MungeStepOutput;
 import ca.sqlpower.validation.swingui.FormValidationHandler;
 
 /**
@@ -47,13 +36,6 @@ import ca.sqlpower.validation.swingui.FormValidationHandler;
  */
 public class SQLInputMungeComponent extends AbstractMungeComponent {
 
-	private JLabel[] labels;
-	private JButton showAll;
-	private JButton hideAll;
-	
-	private static final ImageIcon PLUS_OFF = new ImageIcon(ClassLoader.getSystemResource("icons/plus_off.png"));
-	private static final ImageIcon PLUS_ON = new ImageIcon(ClassLoader.getSystemResource("icons/plus_on.png"));
-	
 	public SQLInputMungeComponent(MungeStep step, FormValidationHandler handler, MatchMakerSession session) {
 		super(step, handler, session);
 	}
@@ -61,106 +43,13 @@ public class SQLInputMungeComponent extends AbstractMungeComponent {
 	@Override
 	protected JPanel buildUI() {
 		JPanel content = new JPanel();
-		
-		try {
-			getStep().open();
-			getStep().close();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		
-		labels = new JLabel[getOutputs().size()];
-		
-		content.setLayout(new BorderLayout());
-
-		JPanel top = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 10));
-		hideAll = new JButton(new AbstractAction("Hide All") {
-
-			public void actionPerformed(ActionEvent e) {
-				for (JLabel label: labels) {
-					hide(label);
-				}
-			}
-			
-		});
-		showAll = new JButton(new AbstractAction("Show All") {
-			
-			public void actionPerformed(ActionEvent e) {
-				for (JLabel label: labels) {
-					show(label);
-				}
-			}
-		});
-		top.add(hideAll);
-		top.add(showAll);
-		
-		JPanel bottom = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 0));
-		int x = 0;
-		for (MungeStepOutput o: getStep().getChildren()) {
-			JLabel lab =new JLabel(o.getName());
-			lab.setToolTipText(o.getName());
-			ToolTipManager toolTipManager = ToolTipManager.sharedInstance();
-			toolTipManager.setInitialDelay(0);
-			
-			lab.addMouseListener(new MouseAdapter(){
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					hideShow((JLabel)e.getSource());
-				}
-				
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					if (((JLabel)e.getSource()).getText().equals("")) {
-						((JLabel)e.getSource()).setIcon(PLUS_ON);
-					}
-				}
-				
-				@Override
-				public void mouseExited(MouseEvent e) {
-					if (((JLabel)e.getSource()).getText().equals("")) {
-						((JLabel)e.getSource()).setIcon(PLUS_OFF);
-					}
-				}
-			});
-			
-			lab.setBorder(BorderFactory.createEtchedBorder());
-			bottom.add(lab);
-			labels[x++] = lab;
-			hide(lab);
-		}
-		
-		content.add(top, BorderLayout.NORTH);
-		content.add(bottom, BorderLayout.SOUTH);
+		content.setLayout(new FlowLayout());
+		content.add(new JButton(new ShowAllOutputNamesAction("Show All")));
+		content.add(new JButton(new HideAllOutputNamesAction("Hide All")));
+		setOutputShowNames(true);
 		return content;
 	}
-	
-	@Override
-	public Point getOutputPosition(int inputNum) {
-		Point orig = super.getOutputPosition(inputNum);
-		if (isExpanded()) {	
-			return new Point(labels[inputNum].getX() + labels[inputNum].getWidth()/2,orig.y);
-		}
-		return orig;
-	}
-	
-	public void hide(JLabel label) {
-		label.setText("");
-		label.setIcon(PLUS_OFF);
-	}
-
-	public void show(JLabel label) {
-		label.setIcon(null);
-		label.setText(label.getToolTipText());
-	}
-	
-	public void hideShow(JLabel label) {
-		if (label.getText().equals("")) {
-			show(label);
-		} else {
-			hide(label);
-		}
-	}
-	
+		
 	@Override
 	public void remove() {
 	}
