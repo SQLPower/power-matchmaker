e757fbae11b65b1466f976ba48f728964ef4a1c4
sean.oczkowski <sean.oczkowski@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-11-01 17:01:40 +0000

    Put in a check to not run a process if the length is less then 2 because the loop would not terminate.
    Set the defaults for the colour and the priority if the process does not have one in the process editor because this was causing errors and the code that did this before was lost somewhere.

--- trunk/src/ca/sqlpower/matchmaker/munge/MungeProcessor.java
+++ trunk/src/ca/sqlpower/matchmaker/munge/MungeProcessor.java
@@ -85,9 +85,15 @@ public class MungeProcessor extends AbstractProcessor {
 			
 			// call until one step gives up
 			boolean finished = false;
+			
+			//stops the process if nothing is going to happen
+			//this will cause an infinite loop if nothing returns false
+			if (processOrder.size() < 2) {
+				finished = true;
+			}
 			while(!finished && !monitorableHelper.isCancelled() && (rowCount == -1 || monitorableHelper.getProgress() < rowCount)) {
 				for (MungeStep step: processOrder) {
-					boolean continuing = step.call() && !monitorableHelper.isCancelled();
+					boolean continuing = !monitorableHelper.isCancelled() && step.call();
 					if (!continuing) {
 						finished = true;
 						break;
