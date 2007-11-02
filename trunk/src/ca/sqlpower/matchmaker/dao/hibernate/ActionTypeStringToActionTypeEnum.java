814573c851517ad47299fd921cdc1c1869cc4da6
jfuerth <jfuerth@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-11-02 15:19:44 +0000

    Fixed up and partially documented the ColumnMergeRules.MergeActionType enum.  It now behaves like a normal enum, but is still lacking some documentation.
    Also modified the hibernate UserType that persists these enum values.  However, there is no need for a custom UserType just for this one enum, so Ben is going to delete that user type and make a new one that works equally well for all enums.

--- trunk/src/ca/sqlpower/matchmaker/dao/hibernate/ActionTypeStringToActionTypeEnum.java
+++ trunk/src/ca/sqlpower/matchmaker/dao/hibernate/ActionTypeStringToActionTypeEnum.java
@@ -72,7 +72,7 @@ public class ActionTypeStringToActionTypeEnum implements UserType {
 			throws HibernateException, SQLException {
 		String typeString = rs.getString(names[0]);
 		if (typeString != null) {
-			return MergeActionType.getActionTypeFromString(typeString);
+			return Enum.valueOf(MergeActionType.class, typeString);
 		} else {
 			return null;
 		}
@@ -81,7 +81,7 @@ public class ActionTypeStringToActionTypeEnum implements UserType {
 	public void nullSafeSet(PreparedStatement st, Object value, int index)
 			throws HibernateException, SQLException {
 		if (value != null) {
-			st.setString(index, ((MergeActionType) value).toString());
+			st.setString(index, ((MergeActionType) value).name());
 		} else {
 			st.setString(index, null);
 		}
