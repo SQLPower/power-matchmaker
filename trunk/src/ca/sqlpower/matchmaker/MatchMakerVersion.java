4e3828434b87795ff20cffc3f772f4211f54ec68
jeffrey@sqlpower.ca <jeffrey@sqlpower.ca@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-11-16 17:16:10 +0000

    Updated MatchMaker version to 0.9.2-alpha

--- trunk/src/ca/sqlpower/matchmaker/MatchMakerVersion.java
+++ trunk/src/ca/sqlpower/matchmaker/MatchMakerVersion.java
@@ -43,7 +43,7 @@ public class MatchMakerVersion extends Task {
      * back to 0 when the minor version changes.  All versions under the same
      * minor version number are fully compatible with each other.
      */
-    public static final String APP_VERSION_TINY   = "1";
+    public static final String APP_VERSION_TINY   = "2";
     
     /**
      * Suffixes indicate pre-release builds.  They normally progress from "alpha"
@@ -54,7 +54,7 @@ public class MatchMakerVersion extends Task {
      * Full releases do not have a suffix.  In that case, the suffix is the empty
      * string (not null).
      */
-    public static final String APP_VERSION_SUFFIX = "";
+    public static final String APP_VERSION_SUFFIX = "alpha";
     
     /**
      * The full version number, formatted as Major.Minor.Tiny[-Suffix].  Note the square
