034ecab4e6e8e81c6a4a0044c9efb6ae6279c675
kevin1219 <kevin1219@10adfead-6038-0410-9fcd-4bb9978d2467>
2007-11-26 17:41:59 +0000

    -Added a opencsv jar package that is to be used in the new munge step. -Added a new munge step that writes the inputs to a csv file. It accepts String inputs only and has options for the separator, quote char, escape char. -Generalized the LogFileNameValidator to a generic file name validator and moved it to the sqlpower library package. -Updated munge step mappings and tests.

--- src/ca/sqlpower/matchmaker/swingui/engine/MatchEnginePanel.java
+++ src/ca/sqlpower/matchmaker/swingui/engine/MatchEnginePanel.java
@@ -54,6 +54,7 @@ import ca.sqlpower.matchmaker.Project;
 import ca.sqlpower.matchmaker.swingui.MatchMakerSwingSession;
 import ca.sqlpower.swingui.BrowseFileAction;
 import ca.sqlpower.swingui.DataEntryPanel;
+import ca.sqlpower.validation.FileNameValidator;
 import ca.sqlpower.validation.Status;
 import ca.sqlpower.validation.ValidateResult;
 import ca.sqlpower.validation.swingui.FormValidationHandler;
@@ -283,7 +284,7 @@ public class MatchEnginePanel implements DataEntryPanel {
 		}
 		File logFile = settings.getLog();
 		logFilePath = new JTextField(logFile.getAbsolutePath());
-		handler.addValidateObject(logFilePath, new LogFileNameValidator());
+		handler.addValidateObject(logFilePath, new FileNameValidator("Log"));
 		
 		browseLogFileAction = new BrowseFileAction(parentFrame, logFilePath);
 		
