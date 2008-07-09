e87b97cce9c2c0907fee1ec15582d5fb66c3c7eb
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2008-07-09 17:13:52 +0000

    Update for the table node renderer so nulls show up in grey and non-nulls show up in the renderer's foreground colour. Previously, all text had to be in the same colour.

--- src/ca/sqlpower/matchmaker/swingui/SourceTableNodeRenderer.java
+++ src/ca/sqlpower/matchmaker/swingui/SourceTableNodeRenderer.java
@@ -20,14 +20,15 @@
 package ca.sqlpower.matchmaker.swingui;
 
 import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
-import ca.sqlpower.architect.SQLColumn;
 import ca.sqlpower.matchmaker.SourceTableRecord;
 import ca.sqlpower.matchmaker.swingui.graphViewer.GraphNodeRenderer;
 
@@ -40,60 +41,110 @@ import ca.sqlpower.matchmaker.swingui.graphViewer.GraphNodeRenderer;
  */
 public class SourceTableNodeRenderer extends DefaultTableCellRenderer implements GraphNodeRenderer<SourceTableRecord> {
 
-    private JTable DUMMY_TABLE;
-    
     /**
-     * The list of columns whose values will be used to choose the display label
-     * for a node.
+     * A simple container for a part of the label. We care about the text, the colour,
+     * and how many pixels wide it will be when painted.
      */
-    private List<SQLColumn> displayColumns;
-    
-    public SourceTableNodeRenderer(List<SQLColumn> displayColumns) {
-        setOpaque(true);
-        setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        this.displayColumns = displayColumns;
+    private static class LabelPart {
+
+        final String text;
+        final Color color;
+        final int width;
+
+        public LabelPart(Color color, String text, int width) {
+            super();
+            this.color = color;
+            this.text = text;
+            this.width = width;
+        }
     }
     
+    /**
+     * The string we show for null values.
+     */
+    private static final String NULL_STRING = "(null)";
+    
+    /**
+     * The string that goes between the values we display.
+     */
+    private static final String SEPARATOR = ", ";
+
+    /**
+     * Since this renderer is just a specialized table cell renderer, we have to provide
+     * a fake parent table to keep the superclass happy. This is that table.
+     */
+    private final JTable DUMMY_TABLE = new JTable(1,1);;
+    
+    /**
+     * The parts of the label to render. Gets set up in {@link #getGraphNodeRendererComponent(SourceTableRecord, boolean, boolean)},
+     * and used by {@link #paintComponent(Graphics)}.
+     */
+    private List<LabelPart> labelParts;
+    
+    /**
+     * Creates a new renderer for source table graph node items.
+     */
+    public SourceTableNodeRenderer() {
+        setOpaque(true);
+    }
+    
+    /**
+     * Sets up this renderer to paint the display values of the given node.
+     */
     public JComponent getGraphNodeRendererComponent(SourceTableRecord node, boolean isSelected, boolean hasFocus) {
-        DUMMY_TABLE = new JTable(1,1);
-    	getTableCellRendererComponent(DUMMY_TABLE, makeLabel(node), isSelected, hasFocus, 0, 0);
+        
+        FontMetrics fm = getFontMetrics(getFont());
+        labelParts = new ArrayList<LabelPart>(node.getDisplayValues().size() * 2 - 1);
+        boolean first = true;
+        for (Object value: node.getDisplayValues()) {
+            if (!first) {
+                labelParts.add(new LabelPart(getForeground(), SEPARATOR, fm.stringWidth(SEPARATOR)));
+            }
+            
+            String label;
+            Color color;
+            if (value == null) {
+                label = NULL_STRING;
+                color = Color.GRAY;
+            } else {
+                label = value.toString();
+                color = getForeground();
+            }
+            labelParts.add(new LabelPart(color, label, fm.stringWidth(label)));
+            first = false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (LabelPart p : labelParts) {
+            sb.append(p.text);
+        }
+
+        // the contents of sb are simply for getting the right preferred size
+    	getTableCellRendererComponent(DUMMY_TABLE, sb.toString(), isSelected, hasFocus, 0, 0);
+    	
         return this;
     }
 
-    /**
-     * Returns a string representation of the given node's display values.
-     * 
-     * IMPORTANT: It also has a side effect of setting the foreground to
-     * grey when any of the values were null.
-     */
-    private String makeLabel(SourceTableRecord node) {
-    	String ret = null;
-    	StringBuilder sb = new StringBuilder();
-    	boolean first = true;
-    	boolean nulls = false;
-    	for (Object value: node.getDisplayValues()) {
-    		if (!first) {
-    			sb.append(", ");
-    		}
-    		
-    		nulls |= (value == null);
-   			sb.append(value == null ? "(null)" : String.valueOf(value));
-    		first = false;
-    	}
-    	ret = sb.toString();
-    	
-    	setForeground(nulls ? Color.GRAY : Color.BLACK);
-    	
-    	if (ret == null || ret.equals("")) return "Error";
-    	return ret;
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        // don't call super.paintComponent() because it will try to draw the string again!
+        g.setColor(getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
+        
+        FontMetrics fm = getFontMetrics(getFont());
+        
+        int x = 0;
+        if (getBorder() != null) {
+            x = getBorder().getBorderInsets(this).left;
+        }
+        int y = fm.getAscent();
+       
+        for (LabelPart p : labelParts) {
+            g.setColor(p.color);
+            g.drawString(p.text, x, y);
+            x += p.width;
+        }
     }
     
-	public List<SQLColumn> getDisplayColumns() {
-		return displayColumns;
-	}
-
-	public void setDisplayColumns(List<SQLColumn> displayColumns) {
-		this.displayColumns = displayColumns;
-	}
-
 }
