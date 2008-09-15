e667647b99b7c18d8c5d6a29e618f8779941bea8
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2008-09-15 15:14:49 +0000

    Removed some strange and ineffective checks from most set methods.

--- src/ca/sqlpower/matchmaker/event/MatchMakerEvent.java
+++ src/ca/sqlpower/matchmaker/event/MatchMakerEvent.java
@@ -60,50 +60,46 @@ public class MatchMakerEvent<S extends MatchMakerObject,C extends MatchMakerObje
 	}
 	
 	public void setSource(S source) {
-		if (this.source != source) {
-			this.source = source;
-		}
+	    this.source = source;
 	}
+	
 	public int[] getChangeIndices() {
 		return changeIndices;
 	}
+	
 	public void setChangeIndices(int[] changeIndeces) {
-		if (this.changeIndices != changeIndeces) {
-			this.changeIndices = changeIndeces;
-		}
+	    this.changeIndices = changeIndeces;
 	}
+	
 	public List<C> getChildren() {
 		return children;
 	}
+	
 	public void setChildren(List<C> children) {
-		if (this.children != children) {
-			this.children = children;
-		}
+	    this.children = children;
 	}
+	
 	public Object getNewValue() {
 		return newValue;
 	}
+	
 	public void setNewValue(Object newValue) {
-		if (this.newValue != newValue) {
-			this.newValue = newValue;
-		}
+	    this.newValue = newValue;
 	}
+	
 	public Object getOldValue() {
 		return oldValue;
 	}
+	
 	public void setOldValue(Object oldValue) {
-		if (this.oldValue != oldValue) {
-			this.oldValue = oldValue;
-		}
+	    this.oldValue = oldValue;
 	}
+	
 	public String getPropertyName() {
 		return propertyName;
 	}
-	public void setPropertyName(String propertyName) {
-		if (this.propertyName != propertyName) {
-			this.propertyName = propertyName;
-		}
-	}
 	
-
+	public void setPropertyName(String propertyName) {
+	    this.propertyName = propertyName;
+	}
 }
