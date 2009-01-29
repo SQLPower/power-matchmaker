cd8661ea786a70e9c0ebb4f33f62964a9c4cbef5
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2009-01-29 23:59:20 +0000

    Further testing and development of the address validation and correction API, which is starting to do useful things but is certainly not solidified yet.

--- src/ca/sqlpower/matchmaker/address/AddressDatabase.java
+++ src/ca/sqlpower/matchmaker/address/AddressDatabase.java
@@ -20,8 +20,16 @@
 package ca.sqlpower.matchmaker.address;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import ca.sqlpower.validation.Status;
+import ca.sqlpower.validation.ValidateResult;
 
 import com.sleepycat.je.DatabaseException;
 import com.sleepycat.je.Environment;
@@ -34,9 +42,11 @@ import com.sleepycat.persist.StoreConfig;
 
 public class AddressDatabase {
 
+    private static final Logger logger = Logger.getLogger(AddressDatabase.class);
+    
     private final File databaseEnvironmentLocation;
     private Environment env;
-    private EntityStore store;
+    private final List<EntityStore> storesToClose = new ArrayList<EntityStore>();
     private PrimaryIndex<String, Municipality> municipalityPK;
     private SecondaryIndex<String, String, Municipality> municipalitySK;
     
@@ -53,20 +63,47 @@ public class AddressDatabase {
         
         StoreConfig storeConfig = new StoreConfig();
         storeConfig.setReadOnly(true);
-        store = new EntityStore(env, "Municipality", storeConfig);
         
+        EntityStore store = new EntityStore(env, "Municipality", storeConfig);
+        storesToClose.add(store);
         municipalityPK = store.getPrimaryIndex(String.class, Municipality.class);
         municipalitySK = store.getSecondaryIndex(municipalityPK, String.class, "alternateNames");
         
+        store = new EntityStore(env, "PostalCode", storeConfig);
+        storesToClose.add(store);
         postalCodePK = store.getPrimaryIndex(String.class, PostalCode.class);
         postalCodeProvince = store.getSecondaryIndex(postalCodePK, String.class, "provinceCode");
         postalCodeMunicipality = store.getSecondaryIndex(postalCodePK, String.class, "municipalityName");
         postalCodeStreet = store.getSecondaryIndex(postalCodePK, String.class, "streetName");
     }
-    
-    public void close() throws DatabaseException {
-        store.close();
-        env.close();
+
+    /**
+     * Tries to close all entity stores and then the BDB environment. Traps and
+     * logs exceptions along the way, because otherwise the larger pieces such
+     * as the whole environment will not get closed.
+     * <p>
+     * Note to people finding this code: This is <i>not</i> in line with our
+     * standard policy for handling exceptions (which is that you have to let
+     * them propagate or show them to a user). Don't take this as an example of
+     * the right way to deal with an exception in MatchMaker code. Think of it
+     * as an exception to the rule about exceptions (ha ha, pun).
+     */
+    public void close() {
+        for (EntityStore store : storesToClose) {
+            try {
+                store.close();
+            } catch (Exception ex) {
+                logger.error("Failed to close entity store " + store + ". Squishing this exception:", ex);
+            }
+        }
+        
+        storesToClose.clear();
+        
+        try {
+            env.close();
+        } catch (Exception ex) {
+            logger.error("Failed to close BDB environment " + env + ". Squishing this exception:", ex);
+        }
     }
     
     /**
@@ -78,14 +115,65 @@ public class AddressDatabase {
         open();
     }
 
-
-    public void correctMunicipality(Address a) {
-        try {
-            Set<Municipality> municipalities = findMunicipality(a.getMunicipality(), a.getProvince());
-            
-            if (municipalities.size() == 1) {
-                a.setMunicipality(municipalities.iterator().next().getOfficialName());
+    public List<ValidateResult> validate(Address a) throws DatabaseException {
+        
+        List<ValidateResult> results = new ArrayList<ValidateResult>();
+        
+        // translate province/state names to official code
+        
+        // translate municipality name to canonical name
+        Set<Municipality> municipalities = findMunicipality(a.getMunicipality(), a.getProvince());
+        if (municipalities.size() == 0) {
+            results.add(ValidateResult.createValidateResult(
+                    Status.FAIL, "Municipality \"" + a.getMunicipality() + "\" does not exist"));
+        } else if (municipalities.size() > 1) {
+            results.add(ValidateResult.createValidateResult(
+                    Status.FAIL, "Municipality \"" + a.getMunicipality() + "\" is ambiguous"));
+        } else {
+            Municipality m = municipalities.iterator().next();
+            if (!m.isNameAcceptable(a.getMunicipality(), a.getPostalCode())) {
+                a.setMunicipality(m.getOfficialName());
             }
+            a.setProvince(m.getProvince());
+            results.add(ValidateResult.createValidateResult(
+                    Status.WARN, "Corrected municipality to " + a.getMunicipality() + ", " + a.getProvince()));
+        }
+        
+        // validate street
+        
+        if (a.getPostalCode() != null) {
+            String pcNormalized = a.getPostalCode().toUpperCase().replaceAll("[^A-Z0-9]", "");
+            PostalCode pc = postalCodePK.get(pcNormalized);
+            
+            // verify province, municipality, street, type, direction, and street number
+            if (pc == null) {
+                results.add(ValidateResult.createValidateResult(
+                        Status.FAIL, "Invalid postal code: " + a.getPostalCode()));
+            } else {
+                if (!pc.getProvinceCode().equals(a.getProvince())) {
+                    results.add(ValidateResult.createValidateResult(
+                            Status.FAIL, "Province code does not agree with postal code"));
+                }
+                if (!pc.getMunicipalityName().equals(a.getMunicipality())) {
+                    results.add(ValidateResult.createValidateResult(
+                            Status.FAIL, "Municipality does not agree with postal code"));
+                }
+                if (pc.getStreetName() != null && !pc.getStreetName().equals(a.getStreet())) {
+                    results.add(ValidateResult.createValidateResult(
+                            Status.FAIL, "Street name does not agree with postal code"));
+                }
+            }
+            
+        } else {
+            // try to find unique postal code match
+        }
+        
+        return results;
+    }
+    
+    public List<ValidateResult> correct(Address a) {
+        try {
+            return validate(a);
         } catch (DatabaseException e) {
             throw new RuntimeException(e);
         }
@@ -99,7 +187,8 @@ public class AddressDatabase {
      * 
      * @param name
      *            The official, valid alternate, or invalid alternate name of a
-     *            municipality
+     *            municipality. If null, no matches will be returned (you get an
+     *            empty set).
      * @param province
      *            the two-letter province code to restrict the search to. If no
      *            provincial restriction is desired, this parameter can be set
@@ -107,6 +196,9 @@ public class AddressDatabase {
      * @return The set of municipalities that match the search criteria
      */
     public Set<Municipality> findMunicipality(String name, String province) throws DatabaseException {
+        if (name == null) {
+            return Collections.emptySet();
+        }
         String upperName = name.toUpperCase();
         
         // TODO check if province code is valid, and ignore if not
