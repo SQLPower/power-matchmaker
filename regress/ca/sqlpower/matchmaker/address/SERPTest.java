7bdbcb4f315af3e5695dd9f7b6f4caef4b1b78c7
mo.jeff <mo.jeff@10adfead-6038-0410-9fcd-4bb9978d2467>
2009-02-26 16:06:44 +0000

    A test case that, with a self-provided address database, and SERP self-test input and result files (not included), will run the Address parser/corrector with the given inputs and compare them to the expected results. Currently, I have the assertions disabled so that running it would run the entire test and display the final percentage of passed tests.

---
+++ regress/ca/sqlpower/matchmaker/address/SERPTest.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2009, SQL Power Group Inc.
+ *
+ * This file is part of Power*MatchMaker.
+ *
+ * Power*MatchMaker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Power*MatchMaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.matchmaker.address;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+
+import ca.sqlpower.validation.ValidateResult;
+
+/**
+ * This test case takes a SERP self-test file and reads the input, parses it and
+ * corrects it, and then compares the results to a SERP self-test result file.
+ * 
+ * NOTE: You have to provide the address database, test, and result files
+ * yourself.
+ */
+public class SERPTest extends TestCase {
+
+	private Logger logger = Logger.getLogger(SERPTest.class);
+	
+	/**
+	 * Path pointing to the directory containing the address database.
+	 */
+	private static final String ADDRESS_DATABASE_PATH = "";
+	
+	/**
+	 * The path to the SERP self test input file.
+	 */
+	private static final String TEST_FILE_PATH = "";
+	
+	/**
+	 * The path to the SERP self test result file.
+	 */
+	private static final String RESULT_FILE_PATH = "";
+	private AddressDatabase addressDB;
+	private Address address;
+	
+    @Override
+    protected void setUp() throws Exception {
+        addressDB = new AddressDatabase(new File(ADDRESS_DATABASE_PATH));
+        address = new Address();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        addressDB.close();
+        super.tearDown();
+    }
+
+    public void testSERP() throws Exception {
+    	File inputFile = new File (TEST_FILE_PATH);
+    	File resultFile = new File (RESULT_FILE_PATH);
+    	BufferedReader in = new BufferedReader(new FileReader(inputFile));
+    	BufferedReader result = new BufferedReader(new FileReader(resultFile));
+
+    	String line;
+    	String resultLine;
+    	boolean firstLineRead = false;
+    	
+    	int numPassed = 0;
+    	int numFailed = 0;
+    	long before = System.currentTimeMillis();
+    	
+    	while (true) {
+    		line = in.readLine();
+    		resultLine = result.readLine();
+    		if (line == null) break;
+    		if (firstLineRead) {
+    			String streetAddress = line.substring(109,159).trim();
+    			String municipality = line.substring(159, 189).trim();
+    			String province = line.substring(189, 191).trim();
+    			String postalCode = line.substring(214).trim();
+    			
+    			logger.debug("Next address is '" + streetAddress + ", " + municipality + ", " + province + ", " + postalCode);
+    			
+    			address = Address.parse(streetAddress, municipality, province, postalCode, "CA");
+    			
+    			List<ValidateResult> results = addressDB.correct(address);
+    			
+    			for (ValidateResult validateResult: results) {
+    				logger.debug(validateResult);
+    			}
+    			
+    			String resultStreetAddress = resultLine.substring(109,159).trim();
+    			String resultMunicipality = resultLine.substring(159, 189).trim();
+    			String resultProvince = resultLine.substring(189, 191).trim();
+    			String resultPostalCode = resultLine.substring(214, 220).trim();
+
+    			boolean failed = false;
+    			
+    			if (!resultStreetAddress.equals(address.getStreetAddress())) {
+    				logger.debug("Street Address is wrong: Expected '" + resultStreetAddress 
+    						+ "' Got '" + address.getStreetAddress() + "'");
+    				failed = true;
+    			}
+    			
+    			if (!resultMunicipality.equals(address.getMunicipality())) {
+    				logger.debug("Municipality is wrong: Expected '" + resultMunicipality 
+    						+ "' Got '" + address.getMunicipality() + "'");
+    				failed = true;
+    			}
+    			
+    			if (!resultProvince.equals(address.getProvince())) {
+    				logger.debug("Province is wrong: Expected '" + resultProvince 
+    						+ "' Got '" + address.getProvince() + "'");
+    				failed = true;
+    			} 
+    			
+    			if (!resultPostalCode.equals(address.getPostalCode())) {
+    				logger.debug("Postal Code is wrong: Expected '" + resultPostalCode 
+    						+ "' Got '" + address.getPostalCode() + "'");
+    				failed = true;
+    			}
+    			
+    			if (failed) {
+    				numFailed++;
+    			} else {
+    				numPassed++;
+    			}
+    			
+    			logger.debug("");
+    			
+//    			assertEquals(resultStreetAddress, address.getStreetAddress());
+//    			assertEquals(resultMunicipality, address.getMunicipality());
+//    			assertEquals(resultProvince, address.getProvince());
+//    			assertEquals(resultPostalCode, address.getPostalCode());
+    		}
+    		firstLineRead = true;
+    	}
+    	
+    	long time = System.currentTimeMillis() - before;
+    	logger.debug("Test took " + time + " ms");
+    	logger.debug("Number of tests passed: " + numPassed);
+    	logger.debug("Number of tests failed: " + numFailed);
+    	double passedPercent = (double) numPassed / (double) (numPassed + numFailed) * 100;
+    	System.out.printf("Percentage of tests passed %.2f: ", passedPercent);
+    	
+    	in.close();
+    	result.close();
+    }
+}
