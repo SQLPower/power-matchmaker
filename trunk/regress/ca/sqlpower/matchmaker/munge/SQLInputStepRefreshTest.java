f2f7df2c995959ce63f00622770361a5c3610ee7
jfuerth <jfuerth@10adfead-6038-0410-9fcd-4bb9978d2467>
2009-03-02 22:21:46 +0000

    * Factored out the general-purpose bits of the incorrectly-named   ProjectRefreshTest into a new class called AbstractRefreshTest.
    * Moved the test cases that were in ProjectRefreshTest into a new
      test class called SQLInputStepRefreshTest, which is a much more
      appropriate name than ProjectRefreshTest.
      
    * Created a new test class called CleanseResultStepRefreshTest
      that tests the same sort of things as SQLInputStepRefreshTest,
      but for the cleanse result step.
      
    * Modified the cleanse result step so it can refresh its inputs
      after the cleansing table has been structurally modified.

--- trunk/regress/ca/sqlpower/matchmaker/munge/SQLInputStepRefreshTest.java
+++ trunk/regress/ca/sqlpower/matchmaker/munge/SQLInputStepRefreshTest.java
@@ -17,99 +17,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
-package ca.sqlpower.matchmaker.util;
-
-import java.io.IOException;
-import java.sql.Connection;
+package ca.sqlpower.matchmaker.munge;
 
 import org.apache.log4j.Logger;
 
-import ca.sqlpower.matchmaker.DBTestUtil;
-import ca.sqlpower.matchmaker.Project;
-import ca.sqlpower.matchmaker.TestingMatchMakerSession;
-import ca.sqlpower.matchmaker.Project.ProjectMode;
+import ca.sqlpower.matchmaker.AbstractRefreshTest;
 import ca.sqlpower.matchmaker.munge.ConcatMungeStep;
 import ca.sqlpower.matchmaker.munge.DateToStringMungeStep;
 import ca.sqlpower.matchmaker.munge.DeDupeResultStep;
-import ca.sqlpower.matchmaker.munge.MungeProcess;
 import ca.sqlpower.matchmaker.munge.MungeStepOutput;
-import ca.sqlpower.matchmaker.munge.SQLInputStep;
-import ca.sqlpower.sql.SPDataSource;
-import ca.sqlpower.sqlobject.DatabaseConnectedTestCase;
-import ca.sqlpower.sqlobject.SQLTable;
 
-public class ProjectRefreshTest extends DatabaseConnectedTestCase {
+/**
+ * Tests the ability of the SQLInputStep to refresh itself when the
+ * columns of the SQLTable it represents have changed.
+ */
+public class SQLInputStepRefreshTest extends AbstractRefreshTest {
 
-    private static final Logger logger = Logger.getLogger(ProjectRefreshTest.class);
-    
-    private TestingMatchMakerSession session;
-    private Project p;
-
-    private SQLInputStep inputStep;
-
-    private MungeProcess mungeProcess;
-    
-    @Override
-    protected SPDataSource getDataSource() throws IOException {
-        // for performance reasons, most MatchMaker tests do not start off with
-        // a clean, empty schema. They reuse the same connection.
-        // this test wants to start fresh with every test method, so we have to
-        // use a slightly different connection spec.
-        SPDataSource ds = new SPDataSource(DBTestUtil.getHSQLDBInMemoryDS());
-        ds.setUrl("jdbc:hsqldb:mem:refresh?shutdown=true");
-        return ds;
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-
-        sqlx(   "CREATE TABLE public.customer (" +
-                " customer_id INT NOT NULL," +
-                " name VARCHAR(40)," +
-                " dob DATE," +
-                " CONSTRAINT customer_pk PRIMARY KEY (customer_id))");
-        
-        SQLTable customerTable = db.getTableByName(null, "PUBLIC", "CUSTOMER");
-        session = new TestingMatchMakerSession() {
-            @Override
-            public Connection getConnection() {
-                try {
-                    return db.getConnection();
-                } catch (Exception ex) {
-                    throw new RuntimeException();
-                }
-            }
-        };
-        session.setDatabase(db);
-        
-        p = new Project();
-        p.setSession(session);
-        p.setType(ProjectMode.FIND_DUPES);
-        p.setName("Testing Project");
-        p.setSourceTable(customerTable);
-        p.setSourceTableIndex(customerTable.getPrimaryKeyIndex());
-        
-        mungeProcess = new MungeProcess();
-        mungeProcess.setName("Simple");
-        p.addMungeProcess(mungeProcess);
-        inputStep = new SQLInputStep();
-        mungeProcess.addChild(inputStep);
-        inputStep.open(logger);
-        inputStep.rollback();
-        inputStep.close();
-        
-        assertEquals(3, inputStep.getChildCount());
-        assertEquals("CUSTOMER_ID", inputStep.getChildren().get(0).getName());
-        assertEquals("NAME", inputStep.getChildren().get(1).getName());
-        assertEquals("DOB", inputStep.getChildren().get(2).getName());
-        
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+    private static final Logger logger = Logger.getLogger(SQLInputStepRefreshTest.class);
     
     /**
      * Tests that SQLInputStep notices when its source table loses a column, and
