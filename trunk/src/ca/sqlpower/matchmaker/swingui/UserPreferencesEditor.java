8edbd5c7f58aba13d82c0f2cbcf7ff087dfdcd33
ms1279 <ms1279@10adfead-6038-0410-9fcd-4bb9978d2467>
2009-03-23 19:56:37 +0000

    Modified the UserPreferences panel. Set the Address Data Correction Path textfield to non-grow. Included a button to pop up a directory chooser dialog to select an address path and a button to check if the address path displayed in the textfield is valid or not.

--- trunk/src/ca/sqlpower/matchmaker/swingui/UserPreferencesEditor.java
+++ trunk/src/ca/sqlpower/matchmaker/swingui/UserPreferencesEditor.java
@@ -19,20 +19,36 @@
 
 package ca.sqlpower.matchmaker.swingui;
 
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Document;
 
+import ca.sqlpower.matchmaker.address.AddressDatabase;
 import ca.sqlpower.sql.SPDataSource;
 import ca.sqlpower.swingui.DataEntryPanel;
 
 import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
+import com.sleepycat.je.DatabaseException;
 
 public class UserPreferencesEditor implements DataEntryPanel {
 
@@ -54,7 +70,13 @@ public class UserPreferencesEditor implements DataEntryPanel {
     private JRadioButton autoLoginRadioButton = new JRadioButton("Automatically connect to this repository");
     private JComboBox autoLoginDataSourceBox = new JComboBox();
     private JRadioButton loginDialogRadioButton = new JRadioButton("Show the Login Dialog");
+    
+    private Document addressDataPathDoc = new DefaultStyledDocument();
     private JTextField addressDataPath = new JTextField();
+    private JFileChooser addressDataPathChooser = new JFileChooser();
+    private JLabel validatePathResult = new JLabel();
+    private JTextArea validatePathExInfo = new JTextArea();
+    private AddressDatabase testPath;
     
     public UserPreferencesEditor(SwingSessionContext context) {
         this.context = context;
@@ -67,9 +89,11 @@ public class UserPreferencesEditor implements DataEntryPanel {
      * in the session context.
      */
     private JPanel buildUI() {
-        FormLayout layout = new FormLayout("pref,4dlu,pref:grow");
-        DefaultFormBuilder fb = new DefaultFormBuilder(layout);
-        fb.setDefaultDialogBorder();
+        FormLayout layout = new FormLayout("pref,4dlu,max(300;min),4dlu,min",
+				"pref,4dlu,pref,4dlu,pref,12dlu,min,4dlu,pref,2dlu,max(40;min)");
+		CellConstraints cc = new CellConstraints();
+		DefaultFormBuilder fb = new DefaultFormBuilder(layout);
+		fb.setDefaultDialogBorder();
         
         ButtonGroup bg = new ButtonGroup();
         bg.add(autoLoginRadioButton);
@@ -94,13 +118,84 @@ public class UserPreferencesEditor implements DataEntryPanel {
         autoLoginDataSourceBox.setSelectedItem(context.getAutoLoginDataSource());
         autoLoginDataSourceBox.setEnabled(context.isAutoLoginEnabled());
         
-        addressDataPath.setText(context.getAddressCorrectionDataPath());
+        validatePathExInfo.setOpaque(false);
+        validatePathExInfo.setEditable(false);
+        validatePathExInfo.setLineWrap(true);
+        validatePathExInfo.setWrapStyleWord(true);
+        validatePathExInfo.setFont(validatePathResult.getFont());
         
-        fb.append("On Startup:", autoLoginRadioButton);
-        fb.append("",            autoLoginDataSourceBox);
-        fb.append("",            loginDialogRadioButton);
-        fb.appendSeparator();
-        fb.append("Address Correction Data Path:", addressDataPath);
+        addressDataPath.setDocument(addressDataPathDoc);
+        addressDataPathDoc.addDocumentListener(new DocumentListener() {
+
+			public void changedUpdate(DocumentEvent e) {
+				
+			}
+
+			public void insertUpdate(DocumentEvent e) {
+				validatePathResult.setText("");
+				validatePathExInfo.setText("");
+			}
+
+			public void removeUpdate(DocumentEvent e) {
+				validatePathResult.setText("");
+				validatePathExInfo.setText("");
+			}
+        	
+        });
+        try {
+			addressDataPathDoc.remove(0, addressDataPathDoc.getLength());
+        	addressDataPathDoc.insertString(0, context.getAddressCorrectionDataPath(), null);
+		} catch (BadLocationException e2) {
+			e2.printStackTrace();
+		}
+
+        JButton selectPath = new JButton(new AbstractAction("...") {
+
+			public void actionPerformed(ActionEvent e) {
+				addressDataPathChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int selected = addressDataPathChooser.showOpenDialog(getPanel());
+				if (selected == JFileChooser.APPROVE_OPTION) {
+					try {
+						addressDataPathDoc.remove(0, addressDataPathDoc.getLength());
+						addressDataPathDoc.insertString(0, addressDataPathChooser.getSelectedFile().getAbsolutePath(), null);
+					} catch (BadLocationException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+        
+
+        JButton validateAddressDataPath = new JButton(new AbstractAction("Test Path") {
+
+			public void actionPerformed(ActionEvent e) {
+				try {
+					testPath = new AddressDatabase(new File(addressDataPath.getText()));
+					validatePathResult.setText("Address Data Path is valid");
+				} catch (DatabaseException e1) {
+					validatePathResult.setText("Invalid Address Data Path");
+					validatePathExInfo.setText(e1.toString());
+				} finally {
+					if (testPath != null) {
+					testPath.close();
+					}
+				}
+			}
+        	
+        });
+       
+		
+        fb.addLabel("On Startup:", cc.xy(1, 1)); 
+        fb.add(autoLoginRadioButton, cc.xyw(3, 1, 2));		
+        fb.add(autoLoginDataSourceBox, cc.xyw(3, 3, 2));
+        fb.add(loginDialogRadioButton, cc.xyw(3, 5, 2));
+        fb.addSeparator("", cc.xyw(1, 6, 5));
+        fb.addLabel("Address Correction Data Path:", cc.xy(1, 7));
+        fb.add(addressDataPath, cc.xy(3, 7));
+        fb.add(selectPath, cc.xy(5, 7));
+        fb.add(validateAddressDataPath, cc.xy(1, 9));
+        fb.add(validatePathResult, cc.xy(3, 9));
+        fb.add(validatePathExInfo, cc.xy(3, 11));
         
         return fb.getPanel();
     }
