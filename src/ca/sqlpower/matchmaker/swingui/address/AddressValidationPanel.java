512babe4dfeb2071d6572eec1d74a2a85ce3787f
thomasobrien95 <thomasobrien95@gmail.com>
2009-03-26 22:04:42 +0000

    If there is an exception when connecting to the database it will be rethrown as a runtime exception instead of diplaying a user prompt. Previously a NPE was being thrown in an undoable edit because only part of this panel was being initialized when a database exception occurred.

--- src/ca/sqlpower/matchmaker/swingui/address/AddressValidationPanel.java
+++ src/ca/sqlpower/matchmaker/swingui/address/AddressValidationPanel.java
@@ -43,7 +43,6 @@ import org.apache.log4j.Logger;
 import ca.sqlpower.matchmaker.address.AddressDatabase;
 import ca.sqlpower.matchmaker.address.AddressPool;
 import ca.sqlpower.matchmaker.address.AddressResult;
-import ca.sqlpower.matchmaker.swingui.MMSUtils;
 import ca.sqlpower.matchmaker.swingui.MatchMakerSwingSession;
 import ca.sqlpower.matchmaker.swingui.NoEditEditorPane;
 import ca.sqlpower.sqlobject.SQLObjectException;
@@ -99,62 +98,59 @@ public class AddressValidationPanel extends NoEditEditorPane {
     public AddressValidationPanel(MatchMakerSwingSession session, AddressPool pool) {
 		try {
 			addressDatabase = new AddressDatabase(new File(session.getContext().getAddressCorrectionDataPath()));
-			this.pool = pool;
-			addressResults = pool.getAddressResults(logger);
+		} catch (DatabaseException e) {
+			throw new RuntimeException("A database exception occured while trying to connect to the Berkley DB", e);
+		} 
+		this.pool = pool;
+		addressResults = pool.getAddressResults(logger);
 
-			Object[] addressArray = addressResults.toArray();
-			for (int i = 0; i < addressArray.length; i++) {
-				AddressResult address =(AddressResult)addressArray[i];
-				allResults.add(0, address);
-				if (!address.getOutputAddress().isEmptyAddress()) {
-					if (address.isValid()) {
-						validResults.add(0, address);
-					} else {
-						invalidResults.add(0, address);
-					}
+		Object[] addressArray = addressResults.toArray();
+		for (int i = 0; i < addressArray.length; i++) {
+			AddressResult address =(AddressResult)addressArray[i];
+			allResults.add(0, address);
+			if (!address.getOutputAddress().isEmptyAddress()) {
+				if (address.isValid()) {
+					validResults.add(0, address);
 				} else {
 					invalidResults.add(0, address);
 				}
+			} else {
+				invalidResults.add(0, address);
 			}
-			
-			needsValidationList = new JList(allResults);
-			needsValidationList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			needsValidationList.addListSelectionListener(new AddressListCellSelectionListener());
-			needsValidationList.setCellRenderer(new AddressListCellRenderer(null, true));
-			JScrollPane addressPane = new JScrollPane(needsValidationList);
-			addressPane.setPreferredSize(new Dimension(250, 1000));
-					
-			validateResultPane = new JPanel();
-			validateResultPane.setLayout(new BoxLayout(validateResultPane, BoxLayout.Y_AXIS));
-			String[] comboBoxOptions = { "Show All", "Show Invalid Results Only", "Show Valid Result Only" }; 
-			displayComboBox = new JComboBox(comboBoxOptions);
-			displayComboBox.addActionListener(new ActionListener() {
+		}
 
-				public void actionPerformed(ActionEvent e) {
-					JComboBox cb = (JComboBox)e.getSource();
-					int index = cb.getSelectedIndex();
-					if (index == 0) {
-						needsValidationList.setModel(allResults);
-					} else if (index == 1) {
-						needsValidationList.setModel(invalidResults);
-					} else {
-						needsValidationList.setModel(validResults);
-					}
+		needsValidationList = new JList(allResults);
+		needsValidationList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		needsValidationList.addListSelectionListener(new AddressListCellSelectionListener());
+		needsValidationList.setCellRenderer(new AddressListCellRenderer(null, true));
+		JScrollPane addressPane = new JScrollPane(needsValidationList);
+		addressPane.setPreferredSize(new Dimension(250, 1000));
+
+		validateResultPane = new JPanel();
+		validateResultPane.setLayout(new BoxLayout(validateResultPane, BoxLayout.Y_AXIS));
+		String[] comboBoxOptions = { "Show All", "Show Invalid Results Only", "Show Valid Result Only" }; 
+		displayComboBox = new JComboBox(comboBoxOptions);
+		displayComboBox.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JComboBox cb = (JComboBox)e.getSource();
+				int index = cb.getSelectedIndex();
+				if (index == 0) {
+					needsValidationList.setModel(allResults);
+				} else if (index == 1) {
+					needsValidationList.setModel(invalidResults);
+				} else {
+					needsValidationList.setModel(validResults);
 				}
-				
-			});			
-			validateResultPane.add(displayComboBox);
-			validateResultPane.add(addressPane);
-			
-	        horizontalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, validateResultPane,
-					new JLabel("To begin address validation, please select an address from the list.",	JLabel.CENTER));
-			setPanel(horizontalSplitPane);
-		} catch (DatabaseException e) {
-			MMSUtils.showExceptionDialog(
-					        getPanel(), 
-					        "A database exception occured while trying to load the invalid addresses", 
-					        e);
-		} 
+			}
+
+		});			
+		validateResultPane.add(displayComboBox);
+		validateResultPane.add(addressPane);
+
+		horizontalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, validateResultPane,
+				new JLabel("To begin address validation, please select an address from the list.",	JLabel.CENTER));
+		setPanel(horizontalSplitPane);
 	}
 
 	@Override
