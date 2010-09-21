cbea82d9ad64004a9f1c6f6cc3d48d6399dc71a3
gtc.cole@gmail.com <gtc.cole@gmail.com@10adfead-6038-0410-9fcd-4bb9978d2467>
2010-09-21 21:14:28 +0000

    Changed this class to steal off the CountingSPListener class as a subclass so I don't have to change this EVERYWHERE in regression tests.

--- branches/spobject-conversion/regress/ca/sqlpower/matchmaker/event/MatchMakerEventCounter.java
+++ branches/spobject-conversion/regress/ca/sqlpower/matchmaker/event/MatchMakerEventCounter.java
@@ -20,63 +20,20 @@
 
 package ca.sqlpower.matchmaker.event;
 
-import ca.sqlpower.matchmaker.MatchMakerObject;
+import ca.sqlpower.object.CountingSPListener;
 
 /**
  *	Get counts of the various match maker event types
  */
-public class MatchMakerEventCounter<T extends MatchMakerObject, C extends MatchMakerObject>
-	implements MatchMakerListener<T, C> {
-
-	private int childrenInsertedCount;
-	private int childrenRemovedCount;
-	private int propertyChangedCount;
-	private int structureChangedCount;
-	private MatchMakerEvent<T, C> lastEvt;
-
-	public void mmChildrenInserted(MatchMakerEvent evt) {
-		childrenInsertedCount++;
-		lastEvt = evt;
-	}
-
-	public void mmChildrenRemoved(MatchMakerEvent evt) {
-		childrenRemovedCount++;
-		lastEvt = evt;
-	}
-
-	public void mmPropertyChanged(MatchMakerEvent evt) {
-		propertyChangedCount++;
-		lastEvt = evt;
-	}
-
-	public void mmStructureChanged(MatchMakerEvent evt) {
-		structureChangedCount++;
-		lastEvt = evt;
-
-	}
-
-	public int getChildrenInsertedCount() {
-		return childrenInsertedCount;
-	}
-
-	public int getChildrenRemovedCount() {
-		return childrenRemovedCount;
-	}
-
-	public int getPropertyChangedCount() {
-		return propertyChangedCount;
-	}
-
-	public int getStructureChangedCount() {
-		return structureChangedCount;
-	}
+public class MatchMakerEventCounter extends CountingSPListener {
 
 	public int getAllEventCounts(){
-		return structureChangedCount + propertyChangedCount + childrenInsertedCount + childrenRemovedCount;
-	}
-
-	public MatchMakerEvent<T, C> getLastEvt() {
-		return lastEvt;
+		return childAddedCount +
+				childRemovedCount +
+				transactionEndedCount +
+				transactionRollbackCount +
+				transactionStartedCount +
+				propertyChangedCount;
 	}
 
 }
