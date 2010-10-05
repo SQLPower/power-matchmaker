9c732f5bb334afda70df7bf9ca20426b17cdaf7a
ThomasObrien95 <ThomasObrien95@10adfead-6038-0410-9fcd-4bb9978d2467>
2010-10-05 22:00:04 +0000

    Corrected this step's persistence tests.

--- branches/spobject-conversion/src/ca/sqlpower/matchmaker/munge/RefinedSoundexMungeStep.java
+++ branches/spobject-conversion/src/ca/sqlpower/matchmaker/munge/RefinedSoundexMungeStep.java
@@ -19,8 +19,14 @@
 
 package ca.sqlpower.matchmaker.munge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.commons.codec.language.RefinedSoundex;
 
+import ca.sqlpower.object.SPObject;
 import ca.sqlpower.object.annotation.Constructor;
 
 /**
@@ -28,6 +34,11 @@ import ca.sqlpower.object.annotation.Constructor;
  */
 public class RefinedSoundexMungeStep extends AbstractMungeStep {
 
+	@SuppressWarnings("unchecked")
+	public static final List<Class<? extends SPObject>> allowedChildTypes = 
+		Collections.unmodifiableList(new ArrayList<Class<? extends SPObject>>(
+				Arrays.asList(MungeStepOutput.class,MungeStepInput.class)));
+	
 	@Constructor
 	public RefinedSoundexMungeStep() {
 		super("Refined Soundex",false);
