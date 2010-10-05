c25129a77260d9b14cbb4361e65feda33f803e57
ThomasObrien95 <thomas@sqlpower.ca>
2010-10-05 21:39:13 +0000

    Fixed the persistence tests for this step.

--- src/ca/sqlpower/matchmaker/munge/LowerCaseMungeStep.java
+++ src/ca/sqlpower/matchmaker/munge/LowerCaseMungeStep.java
@@ -19,6 +19,12 @@
 
 package ca.sqlpower.matchmaker.munge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import ca.sqlpower.object.SPObject;
 import ca.sqlpower.object.annotation.Constructor;
 
 
@@ -28,6 +34,11 @@ import ca.sqlpower.object.annotation.Constructor;
  */
 public class LowerCaseMungeStep extends AbstractMungeStep {
 
+	@SuppressWarnings("unchecked")
+	public static final List<Class<? extends SPObject>> allowedChildTypes = 
+		Collections.unmodifiableList(new ArrayList<Class<? extends SPObject>>(
+				Arrays.asList(MungeStepOutput.class,MungeStepInput.class)));
+	
 	@Constructor
 	public LowerCaseMungeStep() {
 		super("Lower Case",false);
